51ed0bff3dcc9113b62b8321d69fb751ca7c8ddf
Ron Lucke <lucke@elan-ev.de>
2022-04-07 13:12:47 +0000

    fix #786

--- resources/vue/store/courseware/courseware.module.js
+++ resources/vue/store/courseware/courseware.module.js
@@ -898,6 +898,13 @@ export const actions = {
     },
 
     loadTeacherStatus({ dispatch, rootGetters, state, commit, getters }, userId) {
+        const user = rootGetters['users/byId']({ id: userId });
+
+        if (user.attributes.permission === 'root') {
+            commit('setUserIsTeacher', true);
+            return;
+        }
+
         const membershipId = `${state.context.id}_${userId}`;
 
         return dispatch('course-memberships/loadById', { id: membershipId })
