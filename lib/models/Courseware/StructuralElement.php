03515dc8e6331cd06372ed8d43a0f35ab195c589
Elmar Ludwig <elmar.ludwig@uni-osnabrueck.de>
2022-05-05 23:44:49 +0200

    required parameter $with_children follows optional parameter, re #712

--- lib/models/Courseware/StructuralElement.php
+++ lib/models/Courseware/StructuralElement.php
@@ -739,7 +739,7 @@ SQL;
         return $doc;
     }
 
-    private function getElementPdfExport(string $parent_name = '', bool $with_children, $user)
+    private function getElementPdfExport(string $parent_name, bool $with_children, $user)
     {
         if (!$this->canRead($user)) {
             return '';
