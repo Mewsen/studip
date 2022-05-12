bcd4e10ff21e5b12d3d805561cbf7237bd86f693
Michaela Brückner <brueckner@data-quest.de>
2022-05-12 10:03:41 +0200

    Migration, re #853

--- app/views/file/oer_post_upload.php
+++ app/views/file/oer_post_upload.php
@@ -1,8 +1,11 @@
 <form action='<?= $controller->url_for('file/oer_post_upload/' . $file_ref_id)?>'
-      class='default' method='POST' data-dialog="reload-on-close">
+      class='default' method='POST'>
     <?= CSRFProtection::tokenTag() ?>
 
-    <p><?= _('Wenn Sie möchten, können Sie die hochgeladene Datei für den OER Campus bereitstellen.') ?></p>
+    <p><?= _('Die Datei wurde hochgeladen.') ?></p>
+    <span><?= _('Wenn Sie möchten, können Sie die hochgeladene Datei für den OER Campus bereitstellen.') ?></span>
+    <span><?= _('Falls Sie die Datei zu einem späteren Zeitpunkt bereitstellen möchten,
+        wird Ihnen am Vorlesungsende des Semesters eine Nachricht zugeschickt.') ?></span>
 
     <label>
         <input type="radio" name="oer_upload" value="0"/>
