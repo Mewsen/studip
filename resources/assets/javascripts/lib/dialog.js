9c0379da5f49e51ebb4b7697e472e4fad243df2c
Jan-Hendrik Willms <tleilax+studip@gmail.com>
2022-05-12 12:34:19 +0000

    check validity of forms before proceeding, fixes #772
    Closes #772
    
    Merge request studip/studip!412

--- resources/assets/javascripts/lib/dialog.js
+++ resources/assets/javascripts/lib/dialog.js
@@ -667,9 +667,17 @@ Dialog.removeHeaderHandler = function (header) {
 };
 
 Dialog.initialize = function() {
+    function checkValidity(element) {
+        const form = element.closest('form');
+        if (form === null) {
+            return true;
+        }
+        return form.checkValidity();
+    }
+
     // Actual dialog handler
     function dialogHandler(event) {
-        if (!event.isDefaultPrevented()) {
+        if (!event.isDefaultPrevented() && checkValidity(event.target)) {
             var target = $(event.target).closest('[data-dialog]');
             var options = target.data().dialog;
             if (Dialog.fromElement(target, parseOptions(options))) {
@@ -679,7 +687,7 @@ Dialog.initialize = function() {
     }
 
     function clickHandler(event) {
-        if (!event.isDefaultPrevented()) {
+        if (!event.isDefaultPrevented() && checkValidity(event.target)) {
             var element = $(event.target).closest(':submit,input[type="image"]');
             var form = element.closest('form');
             var action = element.attr('formaction');
