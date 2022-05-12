2c8663395981b47c0a88a2e69fbac8ee73d6f132
noackorama <noackorama@gmail.com>
2022-05-12 18:44:42 +0200

    Kleinigkeiten ergänzt

--- app/views/file/oer_post_upload.php
+++ app/views/file/oer_post_upload.php
@@ -1,24 +1,25 @@
-<form action='<?= $controller->url_for('file/oer_post_upload/' . $file_ref_id)?>'
-      class='default' method='POST'>
+<form action="<?= $controller->link_for('file/oer_post_upload/', $file_ref_id)?>"
+       method="post" class="default" data-dialog="reload-on-close">
     <?= CSRFProtection::tokenTag() ?>
 
     <p><?= _('Die Datei wurde hochgeladen.') ?></p>
     <span><?= _('Wenn Sie möchten, können Sie die hochgeladene Datei für den OER Campus bereitstellen.') ?></span>
     <span><?= _('Falls Sie die Datei zu einem späteren Zeitpunkt bereitstellen möchten,
         wird Ihnen am Vorlesungsende des Semesters eine Nachricht zugeschickt.') ?></span>
-
-    <label>
-        <input type="radio" name="oer_upload" value="0"/>
-        <?= _('Nicht für den OER Campus bereitstellen') ?>
-    </label>
-    <label>
-        <input type="radio" name="oer_upload" value="1"/>
-        <?= _('Jetzt für den OER Campus bereitstellen') ?>
-    </label>
-    <label>
-        <input type="radio" name="oer_upload" value="2"/>
-        <?= _('Zu einem späteren Zeitpunkt für den OER Campus bereitstellen') ?>
-    </label>
+    <fieldset>
+        <label>
+            <input type="radio" name="oer_upload" value="0"/>
+            <?= _('Nicht für den OER Campus bereitstellen') ?>
+        </label>
+        <label>
+            <input type="radio" name="oer_upload" value="1"/>
+            <?= _('Jetzt für den OER Campus bereitstellen') ?>
+        </label>
+        <label>
+            <input type="radio" name="oer_upload" value="2"/>
+            <?= _('Zu einem späteren Zeitpunkt für den OER Campus bereitstellen') ?>
+        </label>
+    </fieldset>
     <footer data-dialog-button>
         <?= Studip\Button::create(_("Speichern"))?>
     </footer>
