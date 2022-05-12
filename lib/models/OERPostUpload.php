d0d63dcc04b8fc0064363bc0aabe6f41e05e2ddb
Michaela Brückner <brueckner@data-quest.de>
2022-05-12 10:03:41 +0200

    Migration, re #853

--- lib/models/OERPostUpload.php
+++ lib/models/OERPostUpload.php
@@ -7,8 +7,7 @@ class OERPostUpload extends SimpleORMap
         $config['db_table'] = 'oer_post_upload';
         $config['belongs_to']['file'] = [
             'class_name' => File::class,
-            'foreign_key' => 'id',
-            'on_delete' => 'delete'
+            'foreign_key' => 'id'
         ];
 
         parent::configure($config);
