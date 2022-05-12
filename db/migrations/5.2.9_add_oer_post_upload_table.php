c535d83abea0da706dac88648bf791c506af684a
Michaela Brückner <brueckner@data-quest.de>
2022-05-12 10:03:41 +0200

    Migration, re #853

---
+++ db/migrations/5.2.9_add_oer_post_upload_table.php
@@ -0,0 +1,31 @@
+<?php
+class AddOerPostUploadTable extends Migration
+{
+    public function description()
+    {
+        return "Adds table to create oer upload reminders";
+    }
+
+    public function up()
+    {
+        $db = DBmanager::Get();
+
+        $db->exec("CREATE TABLE IF NOT EXISTS `oer_post_upload` (
+                `file_ref_id` char(32),
+                `user_id` char(32),
+                `reminder_date` int unsigned,
+                `mkdate` int(11) NOT NULL,
+                `chdate` int(11) NOT NULL
+            )
+            ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC
+            ");
+
+    }
+
+    public function down()
+    {
+        $query = "DROP TABLE `oer_post_upload`";
+        DBManager::get()->exec($query);
+    }
+
+}
