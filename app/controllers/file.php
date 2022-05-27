51aae011b65c30feea54746389978c19eddc7de3
Michaela Brückner <brueckner@data-quest.de>
2022-05-27 08:35:45 +0200

    remove OER_SUGGESTION_ENABLED checking, re #853

--- app/controllers/file.php
+++ app/controllers/file.php
@@ -1638,7 +1638,6 @@ class FileController extends AuthenticatedController
                     }
 
                     if (Config::get()->OERCAMPUS_ENABLED
-                        && Config::get()->OER_ENABLE_SUGGESTIONS
                         && $GLOBALS['perm']->have_perm('tutor') ) {
                         if ($file_ref['content_terms_of_use_id'] === 'SELFMADE_NONPUB'
                             || $file_ref['content_terms_of_use_id'] === 'FREE_LICENSE') {
