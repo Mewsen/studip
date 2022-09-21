2a22066991fbc7e6944c85282f5e371ad78f5c5a
Ron Lucke <lucke@elan-ev.de>
2022-09-21 10:53:25 +0000

    Biest #994
    Closes #994 and #1551
    
    Merge request studip/studip!999

--- resources/vue/courseware-index-app.js
+++ resources/vue/courseware-index-app.js
@@ -9,7 +9,7 @@ import Vuex from 'vuex';
 import axios from 'axios';
 import { mapResourceModules } from '@elan-ev/reststate-vuex';
 
-const mountApp = (STUDIP, createApp, element) => {
+const mountApp = async (STUDIP, createApp, element) => {
     const getHttpClient = () =>
         axios.create({
             baseURL: STUDIP.URLHelper.getURL(`jsonapi.php/v1`, {}, true),
@@ -110,6 +110,7 @@ const mountApp = (STUDIP, createApp, element) => {
                     'status-groups',
                     'users',
                     'institutes',
+                    'institute-memberships',
                     'semesters',
                     'sem-classes',
                     'sem-types',
@@ -122,7 +123,7 @@ const mountApp = (STUDIP, createApp, element) => {
 
     store.dispatch('setUrlHelper', STUDIP.URLHelper);
     store.dispatch('setUserId', STUDIP.USER_ID);
-    store.dispatch('users/loadById', {id: STUDIP.USER_ID});
+    await store.dispatch('users/loadById', {id: STUDIP.USER_ID});
     store.dispatch('setHttpClient', httpClient);
 
     store.dispatch('coursewareContext', {
