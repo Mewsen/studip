4f011d17bc998046a08040f716afde2be0e47f7f
Jan-Hendrik Willms <tleilax+studip@gmail.com>
2022-10-19 09:27:19 +0000

    get language from accept header only if no valid is started, fixes #1680, fixes #1394
    Closes #1680 and #1394
    
    Merge request studip/studip!1089

--- lib/classes/JsonApi/Middlewares/Language.php
+++ lib/classes/JsonApi/Middlewares/Language.php
@@ -1,7 +1,6 @@
 <?php
 namespace JsonApi\Middlewares;
 
-use Negotiation\LanguageNegotiator;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
@@ -11,81 +10,15 @@ use Psr\Http\Message\ServerRequestInterface as Request;
  *
  * @author Jan-Hendrik Willms <tleilax+studip@gmail.com>
  */
-class Language
+final class Language
 {
     public function __invoke(Request $request, ResponseInterface $response, callable $next)
     {
-        $language = $this->detectValidLanguageFromRequest($request);
-        $language_before = false;
+        $language = $_SESSION['_language'] ?? get_accepted_languages($request);
 
-        // Set language if detected
-        if ($language) {
-            $language_before = $_SESSION['_language'];
-            $_SESSION['_language'] = $language;
-            setTempLanguage(false, $language);
-        }
+        init_i18n($language);
+        $_SESSION['_language'] = $language;
 
-        $response = $next($request, $response);
-
-        if ($language) {
-            $_SESSION['_language'] = $language_before;
-            restoreLanguage();
-        }
-
-        return $response;
-    }
-
-    /**
-     * Tries to detect a valid installed language from the request.
-     *
-     * @param Request $request
-     * @return string|null The detected language (if any)
-     */
-    private function detectValidLanguageFromRequest(Request $request): ?string
-    {
-        if (!$request->hasHeader('Accept-Language')) {
-            return null;
-        }
-
-        $negotiator = new LanguageNegotiator();
-        $best_language = $negotiator->getBest(
-            $request->getHeaderLine('Accept-Language'),
-            $this->getStudIPLanguagePriorities()
-        );
-
-        if (!$best_language) {
-            return null;
-        }
-
-        return $this->normalizeLanguageForStudIP($best_language->getType());
-    }
-
-    /**
-     * Returns a list of the normalized installed languages for the Stud.IP
-     * system.
-     *
-     * @return array
-     */
-    private function getStudIPLanguagePriorities(): array
-    {
-        return array_map(
-            function ($language) {
-                return str_replace('_', '-', $language);
-            },
-            array_keys($GLOBALS['INSTALLED_LANGUAGES'])
-        );
-    }
-
-    /**
-     * Normalizes the given language string (<language>-<variety>, e.g. de-de)
-     * for Stud.IP (e.g. de_DE).
-     *
-     * @param string $language
-     * @return string
-     */
-    private function normalizeLanguageForStudIP(string $language): string
-    {
-        $tags = explode('-', $language);
-        return $tags[0] . '_' . strtoupper($tags[1]);
+        return $next($request, $response);
     }
 }
