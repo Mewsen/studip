ce3b0ab8973aab612acbab45321abc99d8dfcfdb
Ron Lucke <lucke@elan-ev.de>
2022-10-21 07:15:56 +0000

    fix #1699
    Closes #1699
    
    Merge request studip/studip!1100

--- resources/vue/components/courseware/CoursewareStructuralElement.vue
+++ resources/vue/components/courseware/CoursewareStructuralElement.vue
@@ -1482,16 +1482,22 @@ export default {
         async createElement() {
             let title = this.newChapterName; // this is the title of the new element
             let parent_id = this.currentId; // new page is descandant as default
+            let writeApproval = this.currentElement.attributes['write-approval'];
+            let readApproval = this.currentElement.attributes['read-approval'];
             if (this.errorEmptyChapterName = title.trim() === '') {
                 return;
             }
             if (this.newChapterParent === 'sibling') {
                 parent_id = this.structuralElement.relationships.parent.data.id;
+                writeApproval = [];
+                readApproval = [];
             }
             this.showElementAddDialog(false);
             await this.createStructuralElement({
                 attributes: {
-                    title,
+                    title: title,
+                    'write-approval':  writeApproval,
+                    'read-approval': readApproval
                 },
                 parentId: parent_id,
                 currentId: this.currentId,
