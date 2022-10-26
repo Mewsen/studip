07563813a5bd55a2843c94ae11fd892870782d96
Ron Lucke <lucke@elan-ev.de>
2022-10-26 10:29:46 +0000

    fix #1632
    Closes #1632
    
    Merge request studip/studip!1103

--- lib/models/Courseware/StructuralElement.php
+++ lib/models/Courseware/StructuralElement.php
@@ -785,7 +785,7 @@ SQL;
             $doc->setHeaderTitle(sprintf(_('Courseware von %s'), $this->user->getFullname()));
         }
 
-        if (!self::canRead($user)) {
+        if (!self::canVisit($user)) {
             $doc->addPage();
             $doc->addContent(_('Diese Seite steht Ihnen nicht zur Verfügung!'));
 
@@ -810,7 +810,7 @@ SQL;
 
     private function getElementPdfExport(int $depth, bool $with_children, $user, $doc)
     {
-        if (!$this->canRead($user)) {
+        if (!$this->canVisit($user)) {
             return '';
         }
         $doc->addPage();
