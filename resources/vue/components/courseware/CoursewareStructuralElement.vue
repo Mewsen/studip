68f7db5a71a3679ca4fc7865726b6f474aebb01b
Farbod Zamani <zamani@elan-ev.de>
2022-11-15 12:11:08 +0000

    CW page metadata image fix
    Closes #911
    
    Merge request studip/studip!1160

--- resources/vue/components/courseware/CoursewareStructuralElement.vue
+++ resources/vue/components/courseware/CoursewareStructuralElement.vue
@@ -215,18 +215,18 @@
                             <courseware-tab :name="textEdit.image" :index="2">
                                 <form class="default" @submit.prevent="">
                                     <img
-                                        v-if="image"
+                                        v-if="showPreviewImage"
                                         :src="image"
                                         class="cw-structural-element-image-preview"
                                         :alt="$gettext('Vorschaubild')"
                                     />
-                                    <label v-if="image">
+                                    <label v-if="showPreviewImage">
                                         <button class="button" @click="deleteImage" v-translate>Bild löschen</button>
                                     </label>
                                     <div v-if="uploadFileError" class="messagebox messagebox_error">
                                         {{ uploadFileError }}
                                     </div>
-                                    <label v-if="!image">
+                                    <label v-if="!showPreviewImage">
                                         <translate>Bild hochladen</translate>
                                         <input ref="upload_image" type="file" accept="image/*" @change="checkUploadFile" />
                                     </label>
@@ -529,6 +529,13 @@ export default {
             oerChildren: true,
             containerList: [],
             consumModeTrap: false,
+            additionalText: '',
+
+            publicLink: {
+                passsword: '',
+                'expire-date': ''
+            },
+            deletingPreviewImage: false,
         };
     },
 
@@ -611,6 +618,10 @@ export default {
 
         image() {
             return this.structuralElement.relationships?.image?.meta?.['download-url'] ?? null;
+        },
+
+        showPreviewImage() {
+            return this.image !== null && this.deletingPreviewImage === false;
         },
 
         structuralElementLoaded() {
@@ -948,6 +959,7 @@ export default {
         initCurrent() {
             this.currentElement = _.cloneDeep(this.structuralElement);
             this.uploadFileError = '';
+            this.deletingPreviewImage = false;
         },
         async menuAction(action) {
             switch (action) {
@@ -1012,8 +1024,9 @@ export default {
             }
         },
         deleteImage() {
-            this.deleteImageForStructuralElement(this.currentElement);
-            this.initCurrent();
+            if (!this.deletingPreviewImage) {
+                this.deletingPreviewImage = true;
+            }
         },
         async storeCurrentElement() {
             const file = this.$refs?.upload_image?.files[0];
@@ -1030,6 +1043,9 @@ export default {
                     console.error(error);
                     this.uploadFileError = this.$gettext('Fehler beim Hochladen der Datei.');
                 });
+                await this.loadStructuralElement(this.currentElement.id);
+            } else if (this.deletingPreviewImage) {
+                await this.deleteImageForStructuralElement(this.currentElement);
             }
             this.showElementEditDialog(false);
 
