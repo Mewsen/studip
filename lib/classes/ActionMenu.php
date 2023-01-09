e3881048fe528f2e8293bcea1a99c5f3f3cce607
Jan-Hendrik Willms <tleilax+studip@gmail.com>
2023-01-09 09:41:30 +0000

    set size of icon in action menus only via css, fixes #1953
    Closes #1953
    
    Merge request studip/studip!1295

--- lib/classes/ActionMenu.php
+++ lib/classes/ActionMenu.php
@@ -107,7 +107,7 @@ class ActionMenu
                     'link'       => $url->link,
                     'icon'       => $url->icon,
                     'label'      => $url->label,
-                    'attributes' => $url->attributes
+                    'attributes' => $url->attributes,
                 ];
             } else {
                 $action = [
@@ -118,7 +118,7 @@ class ActionMenu
                     'attributes' => $attributes,
                 ];
             }
-            $index = $index ?: md5($action['link'].json_encode($action['attributes']));
+            $index = $index ?: md5($action['link'] . json_encode($action['attributes']));
             $action['index'] = $index;
             //now insert it possibly at the desired position:
             $before_key = null;
