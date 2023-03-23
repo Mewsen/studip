5ce7d41ab87c59fea7e7a07058ff947eeaecf471
Jan-Hendrik Willms <tleilax+github@gmail.com>
2023-03-23 11:44:24 +0100

    fix broken merge a64c7ee2620a596ba3acc3bc86073d859c1697f9

--- resources/vue/components/courseware/CoursewareStructuralElement.vue
+++ resources/vue/components/courseware/CoursewareStructuralElement.vue
@@ -164,8 +164,8 @@
                                     <span class="cw-sortable-handle"></span>
                                     <span>{{ container.attributes.title }} ({{ container.attributes.width }})</span>
                                 </li>
-                            </transition-group>
-                        </draggable>
+                            </draggable>
+                        </template>
                         <div class="cw-container-sort-buttons">
                             <button class="button accept" @click="storeSort">
                                 <translate>Sortierung speichern</translate>
