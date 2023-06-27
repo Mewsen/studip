05cfdb67a5748e0ed1ab4bf519e25379c354750e
Ron Lucke <lucke@elan-ev.de>
2023-06-27 10:56:41 +0000

    fix #2771
    Closes #2771
    
    Merge request studip/studip!1880

--- resources/vue/store/courseware/courseware.module.js
+++ resources/vue/store/courseware/courseware.module.js
@@ -1093,7 +1093,7 @@ export const actions = {
 
          return items
             .filter(({ membership, course }) => {
-                return course.relationships.courseware;
+                return course.relationships.courseware.data;
             })
             .map(({ course }) => course);
 
