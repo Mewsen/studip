404bf3699d45fb37fb0fe265b4c063c72835c96a
David Siegfried <david.siegfried@uni-vechta.de>
2023-07-14 19:29:36 +0000

    fix redirect on delete, closes #2850
    Closes #2850
    
    Merge request studip/studip!1936

--- app/controllers/questionnaire.php
+++ app/controllers/questionnaire.php
@@ -26,7 +26,7 @@ class QuestionnaireController extends AuthenticatedController
         if (Navigation::hasItem('/contents/questionnaire/overview')) {
             Navigation::activateItem('/contents/questionnaire/overview');
         }
-
+        $this->params = [];
         $this->range_id = null;
         $this->range_type = null;
         if (!$GLOBALS['perm']->have_perm('autor')) {
@@ -64,6 +64,7 @@ class QuestionnaireController extends AuthenticatedController
                 $questionnaire->stop();
             }
         }
+        $this->params = ['redirect' => 'questionnaire/courseoverview'];
         $this->render_action("overview");
     }
 
