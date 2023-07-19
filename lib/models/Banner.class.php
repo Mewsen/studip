9605d3a66e71dd6bf5c954701d478287817408a0
Rasmus Fuhse <fuhse@data-quest.de>
2023-07-19 13:45:27 +0000

    Resolve "Banner sind zu groß"
    Closes #2943
    
    Merge request studip/studip!1983

--- lib/models/Banner.class.php
+++ lib/models/Banner.class.php
@@ -217,6 +217,6 @@ class Banner extends SimpleORMap
         $this->views += 1;
         $this->store();
 
-        return sprintf('<div style="padding: 5px; text-align: center;">%s</div>', $link);
+        return sprintf('<div class="studip-banner">%s</div>', $link);
    }
 }
