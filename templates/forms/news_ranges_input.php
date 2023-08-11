eefe07d80c6adb75e536b5266132f72285dfea91
Elmar Ludwig <elmar.ludwig@uni-osnabrueck.de>
2023-08-11 17:42:20 +0200

    Revert "merging #2365 to 5.2"
    This reverts commit 87d95289df37d0771def0997228119963dcfa36c.

--- templates/forms/news_ranges_input.php
+++ templates/forms/news_ranges_input.php
@@ -3,7 +3,5 @@
                :items="<?= htmlReady(json_encode($items)) ?>"
                :selectable="<?= htmlReady(json_encode($selectable)) ?>"
                :category_order="<?= htmlReady(json_encode($category_order)) ?>"
-               :required="STUDIPFORM_REQUIRED.indexOf('<?= htmlReady($this->name) ?>') !== -1"
-               label="<?= htmlReady($this->title) ?>"
                @input="output => <?= htmlReady($this->name) ?> = output">
 </editable-list>
