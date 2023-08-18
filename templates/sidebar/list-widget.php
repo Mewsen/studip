bbbf544d8fdf911c3c0742d1d6f5ac2cd13f4453
Elmar Ludwig <elmar.ludwig@uni-osnabrueck.de>
2023-08-18 07:35:16 +0000

    add form to OptionsWidget and refactor code a bit, fixes #1474
    Closes #1474
    
    Merge request studip/studip!2044

--- templates/sidebar/list-widget.php
+++ templates/sidebar/list-widget.php
@@ -1,13 +1,16 @@
-<ul class="<?= implode(' ', $css_classes) ?>" aria-label="<?= htmlReady($title) ?>">
-<? foreach ($elements as $index => $element): ?>
-    <? $icon = $element->icon; ?>
-    <? if ($icon && $element instanceof LinkElement && $element->isDisabled()): ?>
-        <? $icon = $icon->copyWithRole('inactive') ?>
-    <? endif ?>
-    <li id="<?= htmlReady($index) ?>"
-        <?= $icon ? 'style="' . $icon->asCSS() .'"' : '' ?>
-        <?= $element->active ? 'class="active"' : '' ?>>
-        <?= $element->render() ?>
-    </li>
-<? endforeach; ?>
-</ul>
+<form method="post">
+    <?= CSRFProtection::tokenTag() ?>
+    <ul class="<?= implode(' ', $css_classes) ?>" aria-label="<?= htmlReady($title) ?>">
+    <? foreach ($elements as $index => $element): ?>
+        <? $icon = $element->icon; ?>
+        <? if ($icon && $element instanceof LinkElement && $element->isDisabled()): ?>
+            <? $icon = $icon->copyWithRole('inactive') ?>
+        <? endif ?>
+        <li id="<?= htmlReady($index) ?>"
+            <?= $icon ? 'style="' . $icon->asCSS() .'"' : '' ?>
+            <?= $element->active ? 'class="active"' : '' ?>>
+            <?= $element->render() ?>
+        </li>
+    <? endforeach; ?>
+    </ul>
+</form>
