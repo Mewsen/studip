af6436af70cfc2422376258954cf96e0aeba46ca
Elmar Ludwig <elmar.ludwig@uni-osnabrueck.de>
2023-08-31 16:06:01 +0000

    don't use OptionsWidget for custom built select elements, fixes #3109
    Closes #3109
    
    Merge request studip/studip!2103

--- app/controllers/calendar/calendar.php
+++ app/controllers/calendar/calendar.php
@@ -86,7 +86,7 @@ class Calendar_CalendarController extends AuthenticatedController
     {
         $tmpl_factory = $this->get_template_factory();
 
-        $filters = new OptionsWidget();
+        $filters = new SidebarWidget();
         $filters->setTitle('Auswahl');
 
         $tmpl = $tmpl_factory->open('calendar/single/_jump_to');
