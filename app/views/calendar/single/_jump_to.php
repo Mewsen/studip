a7e8ede7162395aeca33c69e7720ad2bf493a18f
David Siegfried <david.siegfried@uni-vechta.de>
2023-09-01 09:56:50 +0000

    added code improvement

--- app/views/calendar/single/_jump_to.php
+++ app/views/calendar/single/_jump_to.php
@@ -3,7 +3,7 @@
     <section class="hgroup">
         <?= _('Gehe zu:') ?>
         <input size="10" style="width: 16em;" type="text" id="jmp_date" name="jmp_date" type="text" value="<?= strftime('%x', $atime)?>">
-        <?= Icon::create('accept', 'clickable')->asInput(['class' => 'text-top', 'formaction' => $action_url]) ?>
+        <?= Icon::create('accept')->asInput(['class' => 'text-top', 'formaction' => $action_url]) ?>
     </section>
 
 <script>
