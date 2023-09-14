ff9163335fb838daf44119364e78977f106cb87f
Ron Lucke <lucke@elan-ev.de>
2023-09-14 11:04:39 +0000

    fix #3178
    Closes #3178 and #3179
    
    Merge request studip/studip!2155

--- resources/vue/components/courseware/CoursewareTasksDialogDistribute.vue
+++ resources/vue/components/courseware/CoursewareTasksDialogDistribute.vue
@@ -379,6 +379,7 @@ export default {
             selectedGroups: [],
             bulkSelectGroups: false,
             requirements: [],
+            distributing: false,
         };
     },
     computed: {
@@ -546,6 +547,7 @@ export default {
             copyStructuralElement: 'copyStructuralElement',
             companionError: 'companionError',
             companionSuccess: 'companionSuccess',
+            companionInfo: 'companionInfo',
             loadCourseMemberships: 'course-memberships/loadRelated',
             loadCourseStatusGroups: 'status-groups/loadRelated',
             createTaskGroup: 'createTaskGroup',
@@ -567,6 +569,11 @@ export default {
             this.loadStructuralElement({ id: id, options: { include: 'children' } });
         },
         async distributeTask() {
+            if (this.distributing) {
+                this.companionInfo({ info: this.$gettext('Aufgaben werden bereits verteilt.') });
+                return;
+            }
+            this.distributing = true;
             const taskGroup = {
                 attributes: {
                     title: this.taskTitle,
@@ -600,11 +607,12 @@ export default {
                 solvers = this.selectedGroups.map((id) => ({ type: 'status-groups', id }));
             }
             taskGroup.relationships.solvers.data = solvers;
-
             await this.createTaskGroup({ taskGroup });
             this.companionSuccess({ info: this.$gettext('Aufgaben wurden verteilt.') });
             this.$emit('newtask');
+            this.distributing = false;
             this.setShowTasksDistributeDialog(false);
+            
         },
         validateSolvers() {
             if (
