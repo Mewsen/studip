c6a157f8c75e8205e69ce2c1ce6d126723353e37
Jan-Hendrik Willms <tleilax+studip@gmail.com>
2023-09-20 12:16:01 +0000

    adjust wording, fixes #3208
    Closes #3208
    
    Merge request studip/studip!2176

--- resources/assets/javascripts/studip-ui.js
+++ resources/assets/javascripts/studip-ui.js
@@ -537,7 +537,7 @@ import eventBus from "./lib/event-bus";
             closeText: $gettext('Schließen'),
             prevText: $gettext('Zurück'),
             nextText: $gettext('Vor'),
-            currentText: $gettext('Jetzt'),
+            currentText: $gettext('Heute'),
             monthNames: [
                 $gettext('Januar'),
                 $gettext('Februar'),
@@ -608,7 +608,10 @@ import eventBus from "./lib/event-bus";
         locale.dayNamesMin = locale.dayNamesShort;
 
         $.datepicker.setDefaults(locale);
-        $.timepicker.setDefaults(locale);
+        $.timepicker.setDefaults({
+            ...locale,
+            currentText: $gettext('Jetzt'),
+        });
     });
 
     var DpHideOnScroll = function () {
