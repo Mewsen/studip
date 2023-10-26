1378c0fc2c5ad3379f2c1f701fd81c1ae71bc05f
Michaela Brückner <brueckner@data-quest.de>
2023-10-26 14:51:31 +0200

    error message in right place, re #2660

--- templates/loginform.php
+++ templates/loginform.php
@@ -26,118 +26,121 @@ $show_hidden_login = false;
 <main id="content" class="loginpage">
     <div id="background-desktop" style="background: url(<?= $bg_desktop ?>) no-repeat top left/cover;"></div>
     <div id="background-mobile" style="background: url(<?= $bg_mobile ?>) no-repeat top left/cover;"></div>
-    <? if ($loginerror): ?>
-        <!-- failed login code -->
-        <?= MessageBox::error(_('Bei der Anmeldung trat ein Fehler auf!'), [
-            $error_msg,
-            sprintf(
-                _('Bitte wenden Sie sich bei Problemen an: <a href="mailto:%1$s">%1$s</a>'),
-                $GLOBALS['UNI_CONTACT']
-            )
-        ]) ?>
-    <? endif; ?>
 
-    <?= implode('', PageLayout::getMessages()); ?>
-
-    <div id="loginbox">
-        <header>
-            <h1><?= htmlReady(Config::get()->UNI_NAME_CLEAN) ?></h1>
-        </header>
-
-        <? if (count($GLOBALS['STUDIP_AUTH_PLUGIN']) === 1 && StudipAuthAbstract::isLoginEnabled()) : ?>
-            <?= $this->render_partial('_standard_loginform', [
-                'display' => 'block'
+    <div>
+        <? if ($loginerror): ?>
+            <!-- failed login code -->
+            <?= MessageBox::error(_('Bei der Anmeldung trat ein Fehler auf!'), [
+                $error_msg,
+                sprintf(
+                    _('Bitte wenden Sie sich bei Problemen an: <a href="mailto:%1$s">%1$s</a>'),
+                    $GLOBALS['UNI_CONTACT']
+                )
             ]) ?>
-        <? endif ?>
-        <nav>
-            <ul>
-                <? foreach (Navigation::getItem('/login') as $key => $nav) : ?>
-                    <? if ($nav->isVisible()) : ?>
-                    <? if ($key === 'standard_login') {
-                            if (count($GLOBALS['STUDIP_AUTH_PLUGIN']) === 1 && StudipAuthAbstract::isLoginEnabled()) {
-                                continue;
-                            } else {
-                                $show_hidden_login = true;
+        <? endif; ?>
+
+        <?= implode('', PageLayout::getMessages()); ?>
+
+        <div id="loginbox">
+            <header>
+                <h1><?= htmlReady(Config::get()->UNI_NAME_CLEAN) ?></h1>
+            </header>
+
+            <? if (count($GLOBALS['STUDIP_AUTH_PLUGIN']) === 1 && StudipAuthAbstract::isLoginEnabled()) : ?>
+                <?= $this->render_partial('_standard_loginform', [
+                    'display' => 'block'
+                ]) ?>
+            <? endif ?>
+            <nav>
+                <ul>
+                    <? foreach (Navigation::getItem('/login') as $key => $nav) : ?>
+                        <? if ($nav->isVisible()) : ?>
+                        <? if ($key === 'standard_login') {
+                                if (count($GLOBALS['STUDIP_AUTH_PLUGIN']) === 1 && StudipAuthAbstract::isLoginEnabled()) {
+                                    continue;
+                                } else {
+                                    $show_hidden_login = true;
+                                }
                             }
-                        }
-                        ?>
-                        <? $name_and_title = explode(' - ', $nav->getTitle()) ?>
-                        <li class="login_link">
-                            <? if (is_internal_url($url = $nav->getURL())) : ?>
-                            <? SkipLinks::addLink($name_and_title[0], $url) ?>
-                            <a href="<?= URLHelper::getLink($url, ['cancel_login' => 1]) ?>" id="<?= $nav->getLinkAttributes()['id'] ?>">
-                            <? else : ?>
-                            <a href="<?= htmlReady($url) ?>" target="_blank" rel="noopener noreferrer">
-                            <? endif ?>
-                            <?= htmlReady($name_and_title[0]) ?>
-                            <p>
-                                <?= htmlReady(!empty($name_and_title[1]) ? $name_and_title[1] : $nav->getDescription()) ?>
-                            </p>
-                            </a>
-                        </li>
+                            ?>
+                            <? $name_and_title = explode(' - ', $nav->getTitle()) ?>
+                            <li class="login_link">
+                                <? if (is_internal_url($url = $nav->getURL())) : ?>
+                                <? SkipLinks::addLink($name_and_title[0], $url) ?>
+                                <a href="<?= URLHelper::getLink($url, ['cancel_login' => 1]) ?>" id="<?= $nav->getLinkAttributes()['id'] ?>">
+                                <? else : ?>
+                                <a href="<?= htmlReady($url) ?>" target="_blank" rel="noopener noreferrer">
+                                <? endif ?>
+                                <?= htmlReady($name_and_title[0]) ?>
+                                <p>
+                                    <?= htmlReady(!empty($name_and_title[1]) ? $name_and_title[1] : $nav->getDescription()) ?>
+                                </p>
+                                </a>
+                            </li>
+                        <? endif ?>
+                    <? endforeach ?>
+                </ul>
+            </nav>
+
+            <? if ($show_hidden_login) : ?>
+                <?= $this->render_partial('_standard_loginform', [
+                    'display' => empty($loginerror) ? 'none' : 'block'
+                ]) ?>
+            <? endif ?>
+
+            <footer>
+                <? if ($GLOBALS['UNI_LOGIN_ADD']) : ?>
+                    <div class="uni_login_add">
+                        <?= $GLOBALS['UNI_LOGIN_ADD'] ?>
+                    </div>
+                <? endif; ?>
+
+                <div id="languages">
+                    <? foreach ($GLOBALS['INSTALLED_LANGUAGES'] as $temp_language_key => $temp_language): ?>
+                        <?= Assets::img('languages/' . $temp_language['picture'], ['alt' => $temp_language['name'], 'size' => '24']) ?>
+                        <a href="index.php?set_language=<?= $temp_language_key ?>">
+                            <?= htmlReady($temp_language['name']) ?>
+                        </a>
+                    <? endforeach; ?>
+                </div>
+
+                <div id="contrast">
+                    <? if (isset($_SESSION['contrast'])) : ?>
+                        <?= Icon::create('accessibility')->asImg(24) ?>
+                        <a href="index.php?unset_contrast=1&cancel_login=1"><?= _('Normalen Kontrast aktivieren') ?></a>
+                        <?= tooltipIcon(_('Aktiviert standardmäßige, nicht barrierefreie Kontraste.')); ?>
+                    <? else : ?>
+                        <?= Icon::create('accessibility')->asImg(24) ?>
+                        <a href="index.php?set_contrast=1&cancel_login=1" id="highcontrastlink"><?= _('Hohen Kontrast aktivieren')?></a>
+                        <?= tooltipIcon(_('Aktiviert einen hohen Kontrast gemäß WCAG 2.1. Diese Einstellung wird nach dem Login übernommen.
+                        Sie können sie in Ihren persönlichen Einstellungen ändern.')); ?>
                     <? endif ?>
-                <? endforeach ?>
-            </ul>
-        </nav>
-
-        <? if ($show_hidden_login) : ?>
-            <?= $this->render_partial('_standard_loginform', [
-                'display' => empty($loginerror) ? 'none' : 'block'
-            ]) ?>
-        <? endif ?>
-
-        <footer>
-            <? if ($GLOBALS['UNI_LOGIN_ADD']) : ?>
-                <div class="uni_login_add">
-                    <?= $GLOBALS['UNI_LOGIN_ADD'] ?>
-                </div>
-            <? endif; ?>
-
-            <div id="languages">
-                <? foreach ($GLOBALS['INSTALLED_LANGUAGES'] as $temp_language_key => $temp_language): ?>
-                    <?= Assets::img('languages/' . $temp_language['picture'], ['alt' => $temp_language['name'], 'size' => '24']) ?>
-                    <a href="index.php?set_language=<?= $temp_language_key ?>">
-                        <?= htmlReady($temp_language['name']) ?>
-                    </a>
-                <? endforeach; ?>
-            </div>
-
-            <div id="contrast">
-                <? if (isset($_SESSION['contrast'])) : ?>
-                    <?= Icon::create('accessibility')->asImg(24) ?>
-                    <a href="index.php?unset_contrast=1&cancel_login=1"><?= _('Normalen Kontrast aktivieren') ?></a>
-                    <?= tooltipIcon(_('Aktiviert standardmäßige, nicht barrierefreie Kontraste.')); ?>
-                <? else : ?>
-                    <?= Icon::create('accessibility')->asImg(24) ?>
-                    <a href="index.php?set_contrast=1&cancel_login=1" id="highcontrastlink"><?= _('Hohen Kontrast aktivieren')?></a>
-                    <?= tooltipIcon(_('Aktiviert einen hohen Kontrast gemäß WCAG 2.1. Diese Einstellung wird nach dem Login übernommen.
-                    Sie können sie in Ihren persönlichen Einstellungen ändern.')); ?>
-                <? endif ?>
-            </div>
-
-            <div class="login_info">
-                <div>
-                    <?= _('Aktive Veranstaltungen') ?>:
-                    <?= number_format($num_active_courses, 0, ',', '.') ?>
                 </div>
 
-                <div>
-                    <?= _('Registrierte NutzerInnen') ?>:
-                    <?= number_format($num_registered_users, 0, ',', '.') ?>
-                </div>
+                <div class="login_info">
+                    <div>
+                        <?= _('Aktive Veranstaltungen') ?>:
+                        <?= number_format($num_active_courses, 0, ',', '.') ?>
+                    </div>
 
-                <div>
-                    <?= _('Davon online') ?>:
-                    <?= number_format($num_online_users, 0, ',', '.') ?>
-                </div>
+                    <div>
+                        <?= _('Registrierte NutzerInnen') ?>:
+                        <?= number_format($num_registered_users, 0, ',', '.') ?>
+                    </div>
 
-                <div>
-                    <a href="dispatch.php/siteinfo/show?cancel_login=1">
-                        <?= _('mehr') ?> &hellip;
-                    </a>
+                    <div>
+                        <?= _('Davon online') ?>:
+                        <?= number_format($num_online_users, 0, ',', '.') ?>
+                    </div>
+
+                    <div>
+                        <a href="dispatch.php/siteinfo/show?cancel_login=1">
+                            <?= _('mehr') ?> &hellip;
+                        </a>
+                    </div>
                 </div>
-            </div>
-        </footer>
+            </footer>
+        </div>
     </div>
 
     <? if (Config::get()->LOGIN_FAQ_VISIBILITY && count($faq_entries) > 0) : ?>
