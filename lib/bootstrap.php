af46abf3ead81f6bcf396827158d1a8fb395520f
Stefan Suchi <suchi@data-quest.de>
2023-11-01 13:07:39 +0000

    prepare release 5.1.6

--- lib/bootstrap.php
+++ lib/bootstrap.php
@@ -13,7 +13,7 @@
 const DEFAULT_ENV = 'production';
 
 //software version - please leave it as it is!
-$SOFTWARE_VERSION = '5.1.5';
+$SOFTWARE_VERSION = '5.1.6';
 
 // Store startup time
 $STUDIP_STARTUP_TIME = microtime(true);
