e52179db320054b1ee2bfcea0da28bd640c87d56
David Siegfried <david.siegfried@uni-vechta.de>
2023-11-22 10:06:28 +0000

    prevent php-warnings, closes #3497
    Closes #3497
    
    Merge request studip/studip!2391

--- lib/classes/ActionMenu.php
+++ lib/classes/ActionMenu.php
@@ -285,7 +285,7 @@ class ActionMenu
     protected function getKeyForIndex($index)
     {
         foreach ($this->actions as $key => $value) {
-            if ($value['index'] === $index) {
+            if (!empty($value['index']) && $value['index'] === $index) {
                 return $key;
             }
         }
