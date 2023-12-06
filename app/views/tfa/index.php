6e96651376352667161b0b5cd9e45f137e7c5be9
Jan-Hendrik Willms <tleilax+studip@gmail.com>
2023-12-06 11:26:20 +0000

    fixes #3532
    Closes #3532
    
    Merge request studip/studip!2426

--- app/views/tfa/index.php
+++ app/views/tfa/index.php
@@ -1,7 +1,16 @@
+<?php
+/**
+ * @var TFASecret $secret
+ * @var TfaController $controller
+ * @var bool $own_profile
+ */
+?>
 <p>
     <?= _('Zwei-Faktor-Authentifizierung ist aktiviert') ?>:
     <?= $secret->type == 'app' ? _('Authenticator-App') : _('E-Mail') ?>
 </p>
 <form action="<?= $controller->revoke() ?>" method="post">
-    <?= Studip\Button::createAccept(_('Aufheben')) ?>
+    <?= Studip\Button::createAccept(_('Aufheben'), 'revoke', $own_profile ? [] : [
+        'disabled' => ''
+    ]) ?>
 </form>
