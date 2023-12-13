b9d2a4d869a07a265e886b24694b7b7e722ace3b
Moritz Strohm <strohm@data-quest.de>
2023-12-13 13:40:10 +0100

    fixed errors in migration

--- db/migrations/5.5.1.1_add_lti13a_tables.php
+++ db/migrations/5.5.1.1_add_lti13a_tables.php
@@ -5,7 +5,7 @@ class AddLti13aTables extends Migration
 {
     public function description()
     {
-        return 'Adds tables for the LTI 1.3A functionality';
+        return 'Add tables for the LTI 1.3A functionality.';
     }
 
     protected function up()
@@ -14,7 +14,7 @@ class AddLti13aTables extends Migration
 
         $db->exec(
             "CREATE TABLE IF NOT EXISTS keyrings (
-                id BIGINT NOT NULL AUTO_INCREMENT,
+                id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
                 range_id CHAR(32) NOT NULL,
                 range_type VARCHAR(16) NOT NULL,
                 public_key BLOB(16384) NOT NULL,
@@ -22,10 +22,9 @@ class AddLti13aTables extends Migration
                 passphrase VARCHAR(512) NOT NULL DEFAULT '',
                 mkdate BIGINT(10) NOT NULL DEFAULT '0',
                 chdate BIGINT(10) NOT NULL DEFAULT '0'
-            )
-            ADD PRIMARY KEY (id),
-            ADD INDEX (range_id, range_type)"
+            )"
         );
+        $db->exec("ALTER TABLE `keyrings` ADD INDEX(`range_id`, `range_type`)");
     }
 
     protected function down()
