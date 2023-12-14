b1a9ca5a1a8d24730329ffa8fe8f58ece7691c86
Moritz Strohm <strohm@data-quest.de>
2023-12-14 16:25:00 +0100

    added lit13a/validate_tool_launch action

--- app/controllers/lti13a.php
+++ app/controllers/lti13a.php
@@ -5,12 +5,29 @@ class Lti13aController extends StudipController
 {
     public function platform_auth_action()
     {
-        
+
     }
 
 
     public function oauth2_token_action()
     {
 
+    }
+
+
+    public function validate_tool_launch_action()
+    {
+        //TODO: Create a PSR-7 request object,
+        //fill it with data, send it to the library
+        //and output its response.
+        //See: https://oat-sa.github.io/doc-lti1p3/libraries/lib-lti1p3-core/doc/message/platform-originating-messages/
+
+        /*
+        $request = new \Slim\Psr7\Request(
+            Request::method(),
+            Request::url(),
+
+        );
+        */
     }
 }
