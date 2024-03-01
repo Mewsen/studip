3017370070457589a22b0b7be5efe31508f2f718
Moritz Strohm <strohm@data-quest.de>
2024-03-01 11:55:51 +0000

    calendar/calendar/share_select: changed tile icons and texts, closes #3635
    Closes #3635
    
    Merge request studip/studip!2644

--- app/views/calendar/calendar/share_select.php
+++ app/views/calendar/calendar/share_select.php
@@ -8,12 +8,12 @@
         <a href="<?= $controller->link_for('calendar/calendar/share') ?>"
            data-dialog="size=default">
             <?= Icon::create('group2')->asImg(50) ?>
-            <?= _('Kalender mit anderen Personen teilen') ?>
+            <?= _('Mit Nutzenden teilen') ?>
         </a>
         <a href="<?= $controller->link_for('calendar/calendar/publish') ?>"
            data-dialog="size=auto">
-            <?= Icon::create('globe')->asImg(50) ?>
-            <?= _('Kalender weltweit veröffentlichen') ?>
+            <?= Icon::create('permalink')->asImg(50) ?>
+            <?= _('Link zu diesem Kalender') ?>
         </a>
     </div>
 </section>
