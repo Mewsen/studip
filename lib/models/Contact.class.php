a489ba55b9b3e8f2980c09a87a3630cfc0ec2b4b
David Siegfried <david.siegfried@uni-vechta.de>
2024-03-20 13:00:08 +0000

    cleanup docblock, closes #3839
    Closes #3839
    
    Merge request studip/studip!2708

--- lib/models/Contact.class.php
+++ lib/models/Contact.class.php
@@ -9,11 +9,9 @@
  * @property array $id alias for pk
  * @property string $owner_id database column
  * @property string $user_id database column
- * @property int|null $mkdate database column
  * @property string $calendar_permissions database column
  *     An enum with the possible values "", "READ" and "WRITE".
  *     The empty string specifies that no calendar permissions are granted.
- * @property SimpleORMapCollection|StatusgruppeUser[] $group_assignments has_many StatusgruppeUser
  * @property User $owner belongs_to User
  * @property User $friend belongs_to User
  * @property string $mkdate database column
