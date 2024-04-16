21ce3e12ac9fde9ce9bb881f89b261bf7889f174
Moritz Strohm <strohm@data-quest.de>
2024-04-16 12:18:57 +0000

    messages/write: removed triple text for adding recipients link, fixes #3961
    Closes #3961
    
    Merge request studip/studip!2857
    
    
    (cherry picked from commit 1c0302b5a90e6388f7e7122e71e18601111816b0)
    
    fa0e9a4e messages/write: removed triple text for adding recipients link
    bf3559b6 do not set title in MultiPersonSearch
    5009117a Apply 1 suggestion(s) to 1 file(s)

--- lib/classes/MultiPersonSearch.class.php
+++ lib/classes/MultiPersonSearch.class.php
@@ -528,9 +528,9 @@ class MultiPersonSearch {
      * sets default values of the internal variables.
      */
     private function setDefaultValues() {
-        $this->title = _('Personen hinzufügen');
+        $this->title = '';
         $this->description = _('Bitte wählen Sie aus, wen Sie hinzufügen möchten.');
-        $this->linkIconPath = Icon::create("add", "clickable", ['title' => _('Personen hinzufügen')]);
+        $this->linkIconPath = Icon::create('add');
     }
 
     /**
