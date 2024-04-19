3057a3cb7774089c3b604f7b1a34927e217840d2
Thomas Hackl <hackl@data-quest.de>
2024-04-19 07:42:14 +0000

    Resolve "Mangelnde Tastatur-Bedienbarkeit im Veranstaltungsverzeichnis"
    Closes #4031
    
    Merge request studip/studip!2886

--- resources/vue/components/tree/StudipTreeList.vue
+++ resources/vue/components/tree/StudipTreeList.vue
@@ -91,9 +91,9 @@
             <tbody>
                 <tr v-for="(course) in courses" :key="course.id" class="studip-tree-child studip-tree-course">
                     <td>
-                        <a :href="courseUrl(course.id)"
-                           :title="$gettextInterpolate($gettext('Zur Veranstaltung %{ course }'),
-                                { course: course.attributes.title })">
+                        <a :href="courseUrl(course.id)" tabindex="0"
+                           :title="$gettextInterpolate($gettext('Zur Veranstaltung %{ title }'),
+                                { title: course.attributes.title })">
                             <studip-icon shape="seminar" :size="26"></studip-icon>
                             <template v-if="course.attributes['course-number']">
                                 {{ course.attributes['course-number'] }}
