0d766d2f811526fb7071130fdba895000528f5ed
Rasmus Fuhse <fuhse@data-quest.de>
2024-04-30 09:32:28 +0000

    Resolve "Cronjobs laufen nicht"
    Closes #4095
    
    Merge request studip/studip!2941

--- lib/classes/CronjobScheduler.class.php
+++ lib/classes/CronjobScheduler.class.php
@@ -247,8 +247,8 @@ class CronjobScheduler
         }
 
         // Find all schedules that are due to execute and which task is active
-        $temp = CronjobSchedule::findBySQL('active = 1 AND next_execution <= UNIX_TIMESTAMP() '
-                                          .'ORDER next_execution');
+        $temp = CronjobSchedule::findBySQL('`active` = 1 AND `next_execution` <= UNIX_TIMESTAMP() '
+                                          .'ORDER BY `next_execution`');
         $schedules = array_filter($temp, function ($schedule) { return $schedule->task->active; });
 
         if (count($schedules) === 0) {
