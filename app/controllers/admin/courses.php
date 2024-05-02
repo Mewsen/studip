324618e947f0a04532aa7c1b75f7a77daca831ab
Rasmus Fuhse <fuhse@data-quest.de>
2024-05-02 12:43:59 +0000

    Resolve "Einrichtungsfilter zeigt falschen Wert bei "Einrichtung + Institute""
    Closes #4051
    
    Merge request studip/studip!2947

--- app/controllers/admin/courses.php
+++ app/controllers/admin/courses.php
@@ -325,6 +325,9 @@ class Admin_CoursesController extends AuthenticatedController
         $institut_id = $configuration->MY_INSTITUTES_DEFAULT && $configuration->MY_INSTITUTES_DEFAULT !== 'all'
                      ? $configuration->MY_INSTITUTES_DEFAULT
                      : null;
+        if ($configuration->MY_INSTITUTES_INCLUDE_CHILDREN) {
+            $institut_id .= '_withinst';
+        }
 
         $filters = array_merge(
             array_merge(...PluginEngine::sendMessage(AdminCourseWidgetPlugin::class, 'getFilters')),
@@ -523,6 +526,14 @@ class Admin_CoursesController extends AuthenticatedController
             'course_type'    => 'MY_COURSES_TYPE_FILTER',
             'institut_id'    => 'MY_INSTITUTES_DEFAULT',
         ];
+
+        $config->store(
+            'MY_INSTITUTES_INCLUDE_CHILDREN',
+            str_contains($filters['institut_id'], '_') ? 1 : 0
+        );
+        if ($config->MY_INSTITUTES_INCLUDE_CHILDREN) {
+            $filters['institut_id'] = substr($filters['institut_id'], 0, strpos($filters['institut_id'], '_'));
+        }
 
         foreach ($mapping as $key => $field) {
             if (isset($filters[$key])) {
@@ -1537,6 +1548,7 @@ class Admin_CoursesController extends AuthenticatedController
                     $institut['Institut_id'],
                     (!$institut['is_fak'] ? ' ' : '') . $institut['Name'],
                     $GLOBALS['user']->cfg->MY_INSTITUTES_DEFAULT === $institut['Institut_id']
+                        && !$GLOBALS['user']->cfg->MY_INSTITUTES_INCLUDE_CHILDREN
                 ),
                 'select-' . $institut['Institut_id']
             );
@@ -1551,7 +1563,8 @@ class Admin_CoursesController extends AuthenticatedController
                     new SelectElement(
                         $institut['Institut_id'] . '_withinst', //_withinst = with institutes
                         ' ' . $institut['Name'] . ' +' . _('Institute'),
-                        ($GLOBALS['user']->cfg->MY_INSTITUTES_DEFAULT === $institut['Institut_id'] && $GLOBALS['user']->cfg->MY_INSTITUTES_INCLUDE_CHILDREN)
+                        $GLOBALS['user']->cfg->MY_INSTITUTES_DEFAULT === $institut['Institut_id']
+                            && $GLOBALS['user']->cfg->MY_INSTITUTES_INCLUDE_CHILDREN
                     ),
                     'select-' . $institut['Name'] . '-with_institutes'
                 );
@@ -1589,6 +1602,9 @@ class Admin_CoursesController extends AuthenticatedController
     private function getStgteilSelector($institut_id = null)
     {
         $institut_id = $institut_id ?: $GLOBALS['user']->cfg->MY_INSTITUTES_DEFAULT;
+        if (str_contains($institut_id, '_')) {
+            $institut_id = substr($institut_id, 0, strpos($institut_id, '_'));
+        }
         $stgteile = StudiengangTeil::getAllEnriched('fach_name', 'ASC', ['mvv_fach_inst.institut_id' => $institut_id]);
         $list = new SelectWidget(_('Studiengangteil'), $this->url_for('admin/courses/set_selection'), 'stgteil_select');
         if (!$institut_id || $institut_id === 'all') {
@@ -1691,6 +1707,9 @@ class Admin_CoursesController extends AuthenticatedController
     private function getTeacherWidget($institut_id = null)
     {
         $institut_id = $institut_id ?: $GLOBALS['user']->cfg->MY_INSTITUTES_DEFAULT;
+        if (str_contains($institut_id, '_')) {
+            $institut_id = substr($institut_id, 0, strpos($institut_id, '_'));
+        }
         $teachers = DBManager::get()->fetchAll("
                 SELECT auth_user_md5.*, user_info.*
                 FROM auth_user_md5
