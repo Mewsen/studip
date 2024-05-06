e7bfc2e80046cdbe28122c8430d9ce9dfdafb5a1
Jan-Hendrik Willms <tleilax+studip@gmail.com>
2024-05-06 09:03:33 +0000

    fixes #3977
    Closes #3977
    
    Merge request studip/studip!2945

---
+++ db/migrations/5.1.57_cleanup_tool_activations.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @see https://gitlab.studip.de/studip/studip/-/issues/3977
+ */
+return new class extends Migration
+{
+    public function description()
+    {
+        return 'Removes invalid tool activations (that are no longer connected '
+            . 'to a StandardPlugin';
+    }
+
+    protected function up()
+    {
+        $query = "DELETE FROM `tools_activated`
+                  WHERE `plugin_id` NOT IN (
+                      SELECT `pluginid`
+                      FROM `plugins`
+                      WHERE FIND_IN_SET(?, `plugintype`)
+                  )";
+        DBManager::get()->execute($query, [StandardPlugin::class]);
+    }
+};
