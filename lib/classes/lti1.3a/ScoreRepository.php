e2c66086dc502ee78ecce1b617052e11b0ad15b8
Moritz Strohm <strohm@data-quest.de>
2024-05-21 16:11:23 +0200

    added basic implementation of ScoreRepository::save

--- lib/classes/lti1.3a/ScoreRepository.php
+++ lib/classes/lti1.3a/ScoreRepository.php
@@ -10,6 +10,21 @@ class ScoreRepository implements ScoreRepositoryInterface
 
     public function save(ScoreInterface $score): ScoreInterface
     {
-        // TODO: Implement save() method.
+        $user_id = $score->getUserIdentifier();
+        $definition_id = $score->getLineItemIdentifier();
+
+        $grade = \Grading\Instance::findBySQL(
+            '`definition_id` = :definition_id AND `user_id` = :user_id',
+            ['definition_id' => $definition_id, 'user_id' => $user_id]
+        );
+        if (!$grade) {
+            $grade = new \Grading\Instance();
+            $grade->definition_id = $definition_id;
+            $grade->user_id       = $user_id;
+        }
+        $grade->rawgrade = $score->getScoreGiven();
+        $grade->feedback = $score->getComment();
+        $grade->store();
+        return $score;
     }
 }
