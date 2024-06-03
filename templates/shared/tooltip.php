23b47edc40c9e5e1a2144cf8288581ce92bc40bd
Jan-Hendrik Willms <tleilax+studip@gmail.com>
2024-06-03 12:32:36 +0000

    fixes #4251
    Closes #4251
    
    Merge request studip/studip!3081

--- templates/shared/tooltip.php
+++ templates/shared/tooltip.php
@@ -1,4 +1,11 @@
+<?php
+/**
+ * @var bool $important
+ * @var bool $html
+ * @var string $text
+ */
+?>
 <span class="tooltip tooltip-icon <? if ($important) echo 'tooltip-important'; ?>"
-      tabindex="0" aria-label="<?= $html ? kill_format($text) : htmlReady($text) ?>">
+      tabindex="0" aria-label="<?= $html ? htmlReady(strip_tags($text)) : htmlReady($text) ?>">
     <span class="tooltip-content"><?= $html ? $text : htmlReady($text) ?></span>
 </span>
