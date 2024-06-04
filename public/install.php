c0791ae86fc89c61a140bead95d231631500bf8a
Jan-Hendrik Willms <tleilax+studip@gmail.com>
2024-06-04 07:12:54 +0000

    fixes #4254
    Closes #4254
    
    Merge request studip/studip!3085

--- public/install.php
+++ public/install.php
@@ -15,36 +15,37 @@ if (file_exists($GLOBALS['STUDIP_BASE_PATH'] . '/config/config_local.inc.php')
 
 set_include_path($GLOBALS['STUDIP_BASE_PATH']);
 
-require_once 'composer/autoload.php';
-require_once 'lib/visual.inc.php';
-require_once 'lib/classes/URLHelper.php';
-require_once 'lib/classes/LayoutMessage.interface.php';
-require_once 'lib/classes/MessageBox.class.php';
-require_once 'lib/classes/Request.class.php';
-require_once 'lib/classes/Interactable.class.php';
-require_once 'lib/classes/Button.class.php';
-require_once 'lib/classes/LinkButton.class.php';
-require_once 'lib/classes/StudipInstaller.php';
-require_once 'lib/classes/SystemChecker.php';
-require_once 'lib/classes/Markup.class.php';
-require_once 'lib/exceptions/AccessDeniedException.php';
-require_once 'lib/flexi/Factory.php';
-require_once 'lib/flexi/PhpTemplate.php';
-require_once 'lib/flexi/Template.php';
-require_once 'lib/flexi/TemplateNotFoundException.php';
-require_once 'lib/trails/Controller.php';
-require_once 'lib/trails/Dispatcher.php';
-require_once 'lib/trails/Exception.php';
-require_once 'lib/trails/Flash.php';
-require_once 'lib/trails/Inflector.php';
-require_once 'lib/trails/Response.php';
-require_once 'lib/trails/Exceptions/DoubleRenderError.php';
-require_once 'lib/trails/Exceptions/MissingFile.php';
-require_once 'lib/trails/Exceptions/RoutingError.php';
-require_once 'lib/trails/Exceptions/SessionRequiredException.php';
-require_once 'lib/trails/Exceptions/UnknownAction.php';
-require_once 'lib/trails/Exceptions/UnknownController.php';
-require_once 'vendor/phpass/PasswordHash.php';
+require 'composer/autoload.php';
+require 'lib/visual.inc.php';
+require 'lib/functions.php';
+require 'lib/classes/URLHelper.php';
+require 'lib/classes/LayoutMessage.interface.php';
+require 'lib/classes/MessageBox.class.php';
+require 'lib/classes/Request.class.php';
+require 'lib/classes/Interactable.class.php';
+require 'lib/classes/Button.class.php';
+require 'lib/classes/LinkButton.class.php';
+require 'lib/classes/StudipInstaller.php';
+require 'lib/classes/SystemChecker.php';
+require 'lib/classes/Markup.class.php';
+require 'lib/exceptions/AccessDeniedException.php';
+require 'lib/flexi/Factory.php';
+require 'lib/flexi/Template.php';
+require 'lib/flexi/PhpTemplate.php';
+require 'lib/flexi/TemplateNotFoundException.php';
+require 'lib/trails/Controller.php';
+require 'lib/trails/Dispatcher.php';
+require 'lib/trails/Exception.php';
+require 'lib/trails/Flash.php';
+require 'lib/trails/Inflector.php';
+require 'lib/trails/Response.php';
+require 'lib/trails/Exceptions/DoubleRenderError.php';
+require 'lib/trails/Exceptions/MissingFile.php';
+require 'lib/trails/Exceptions/RoutingError.php';
+require 'lib/trails/Exceptions/SessionRequiredException.php';
+require 'lib/trails/Exceptions/UnknownAction.php';
+require 'lib/trails/Exceptions/UnknownController.php';
+require 'vendor/phpass/PasswordHash.php';
 
 // Mock gettext functions if extension is not available
 if (!function_exists('_')) {
