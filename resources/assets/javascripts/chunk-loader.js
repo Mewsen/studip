7d4fc03ec7ac868b231ff06e9442a8c6bd1fd0f9
Jan-Hendrik Willms <tleilax+studip@gmail.com>
2024-06-14 12:41:26 +0000

    fixes #4293
    Merge request studip/studip!3106

--- resources/assets/javascripts/chunk-loader.js
+++ resources/assets/javascripts/chunk-loader.js
@@ -1,5 +1,4 @@
-/*jslint esversion: 6*/
-STUDIP.loadScript = function (script_name) {
+export const loadScript = function (script_name) {
     return new Promise(function (resolve, reject) {
         let script = document.createElement('script');
         script.src = `${STUDIP.ASSETS_URL}${script_name}`;
