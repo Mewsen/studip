e84d398fb21acc5be3ea296e9284d88496c29563
Jan-Hendrik Willms <tleilax+github@gmail.com>
2024-06-20 13:23:55 +0200

    Revert "fix typo in oauth2 autorize controller, fixes #4333"
    This reverts commit b04a8f60a9750dc4e152a180cb2a0ca1994ec54c.

--- app/controllers/api/oauth2/authorize.php
+++ app/controllers/api/oauth2/authorize.php
@@ -55,7 +55,7 @@ class Api_Oauth2_AuthorizeController extends OAuth2Controller
         if ('nobody' === $GLOBALS['user']->id && 'Standard' !== $authPlugin && !Request::option('sso')) {
             $queryParams = $psrRequest->getQueryParams();
             $queryParams['sso'] = strtolower($authPlugin);
-            $this->redirect($this->url_for('api/oauth2/authorize', $queryParams));
+            $this->redirect($this->url_for('api/oaut2/authorize', $queryParams));
 
             return;
         } else {
