b497ab49f2ac2fc53ecab2c04b79515078569f9e
Murtaza Sultani <sultani@data-quest.de>
2024-07-10 17:05:01 +0200

    Add seconde optional pram to findTopFolder with default value of 'RootFolder'

--- lib/filesystem/FileManager.php
+++ lib/filesystem/FileManager.php
@@ -1667,7 +1667,7 @@ class FileManager
      */
     public static function getInboxFolder(User $user)
     {
-        $top_folder = Folder::findTopFolder($user->id, 'user');
+        $top_folder = Folder::findTopFolder($user->id);
         if (!$top_folder) {
             return null;
         }
@@ -1710,7 +1710,7 @@ class FileManager
      */
     public static function getOutboxFolder(User $user)
     {
-        $top_folder = Folder::findTopFolder($user->id, 'user');
+        $top_folder = Folder::findTopFolder($user->id);
         if (!$top_folder) {
             return null;
         }
