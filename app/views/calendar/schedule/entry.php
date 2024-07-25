30b5a5fc77ec71b21701c1a2281c4001d4f94e4d
Moritz Strohm <strohm@data-quest.de>
2024-07-25 12:24:47 +0200

    removed old schedule controller and its views

--- app/views/calendar/schedule/entry.php
+++
@@ -1,9 +0,0 @@
-<? if (!empty($show_entry) && in_array($show_entry['type'], ['sem', 'virtual'])): ?>
-    <?= $this->render_partial('calendar/schedule/_entry_course.php') ?>
-    <? unset($show_entry) ?>
-<? elseif (!empty($show_entry) && $show_entry['type'] === 'inst'): ?>
-    <?= $this->render_partial('calendar/schedule/_entry_inst.php') ?>
-    <? unset($show_entry) ?>
-<? else : ?>
-    <?= $this->render_partial('calendar/schedule/_entry_schedule.php') ?>
-<? endif ?>
