7a40d1ad877744fcb78c9fb957d6736483e5333f
Jan-Hendrik Willms <tleilax+studip@gmail.com>
2024-08-08 06:50:26 +0000

    remove calls to removed file, fixes #4468
    Closes #4468
    
    Merge request studip/studip!3255

--- db/migrations/1.49_step_00150_studygroups.php
+++ db/migrations/1.49_step_00150_studygroups.php
@@ -1,5 +1,4 @@
-<?
-require_once 'lib/admission.inc.php';
+<?php
 class Step00150Studygroups extends Migration
 {
 
@@ -11,9 +10,9 @@ class Step00150Studygroups extends Migration
     function up ()
     {
         // (1) Add a new dozent who is used as default dozent for all studygroups
-        DBManager::get()->query("INSERT IGNORE INTO auth_user_md5 (user_id, username, password, perms, Vorname, Nachname, Email, locked) VALUES (MD5('studygroup_dozent'),'studygroup_dozent','0c6fe1b07e3aca7ee6387f87dc8370eb','dozent','','','',1)"); 
-        DBManager::get()->query("INSERT IGNORE INTO user_info SET user_id =MD5('studygroup_dozent')");  
-    
+        DBManager::get()->query("INSERT IGNORE INTO auth_user_md5 (user_id, username, password, perms, Vorname, Nachname, Email, locked) VALUES (MD5('studygroup_dozent'),'studygroup_dozent','0c6fe1b07e3aca7ee6387f87dc8370eb','dozent','','','',1)");
+        DBManager::get()->query("INSERT IGNORE INTO user_info SET user_id =MD5('studygroup_dozent')");
+
         // (2) Allocate some space in the config-table
         DBManager::get()->query("ALTER TABLE `config` CHANGE `value` `value` TEXT NOT NULL");
 
@@ -25,7 +24,7 @@ class Step00150Studygroups extends Migration
             VALUES ( MD5( 'STUDYGROUPS_ENABLE' ) , '', 'STUDYGROUPS_ENABLE',
             '0', '1', 'boolean', 'global', '', '0', UNIX_TIMESTAMP( ) , UNIX_TIMESTAMP( ) ,
             'Studiengruppen', '', '')");
-        
+
         $terms = "Mir ist bekannt, dass ich die Gruppe nicht zu rechtswidrigen Zwecken nutzen darf. Dazu zählen u.a. Urheberrechtsverletzungen, Beleidigungen und andere Persönlichkeitsdelikte.
 
 Ich erkläre mich damit einverstanden, dass Admins die Inhalte der Gruppe zu Kontrollzwecken einsehen dürfen.";
@@ -60,15 +59,14 @@ Ich erkläre mich damit einverstanden, dass Admins die Inhalte der Gruppe zu Kon
     function down ()
     {
         // (1) Remove studygroup_dozent
-        DBManager::get()->query("DELETE FROM auth_user_md5 WHERE user_id = MD5('studygroup_dozent')"); 
-        DBManager::get()->query("DELETE FROM user_info WHERE user_id = MD5('studygroup_dozent')");  
+        DBManager::get()->query("DELETE FROM auth_user_md5 WHERE user_id = MD5('studygroup_dozent')");
+        DBManager::get()->query("DELETE FROM user_info WHERE user_id = MD5('studygroup_dozent')");
         // (2) remove config entries
-        DBManager::get()->query("DELETE FROM config WHERE config_id = MD5('STUDYGROUPS_ENABLE')");  
-        DBManager::get()->query("DELETE FROM config WHERE config_id = MD5('STUDYGROUP_DEFAULT_INST')");  
+        DBManager::get()->query("DELETE FROM config WHERE config_id = MD5('STUDYGROUPS_ENABLE')");
+        DBManager::get()->query("DELETE FROM config WHERE config_id = MD5('STUDYGROUP_DEFAULT_INST')");
         DBManager::get()->query("DELETE FROM config WHERE config_id = MD5('STUDYGROUP_SETTINGS')");
-        DBManager::get()->query("DELETE FROM config WHERE config_id = MD5('STUDYGROUP_TERMS')");  
+        DBManager::get()->query("DELETE FROM config WHERE config_id = MD5('STUDYGROUP_TERMS')");
         // (3) restore config-table schema
         DBManager::get()->query("ALTER TABLE config CHANGE value value varchar(255) NOT NULL");
     }
 }
-?>
