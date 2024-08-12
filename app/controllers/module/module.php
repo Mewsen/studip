9000ad711c86af71e16d196ac3842721d86e7b2d
Jan-Hendrik Willms <tleilax+studip@gmail.com>
2024-08-12 09:32:16 +0000

    jump to, open and mark module as selected after storing changes, fixes #4473
    Closes #4473
    
    Merge request studip/studip!3261

--- app/controllers/module/module.php
+++ app/controllers/module/module.php
@@ -101,6 +101,7 @@ class Module_ModuleController extends MVVController
         $this->count = Modul::getCount($this->filter);
         $this->show_sidebar_search = true;
         $this->show_sidebar_filter = true;
+        $this->modul_id = Request::option('modul_id');
         $this->setSidebar();
     }
 
@@ -323,7 +324,7 @@ class Module_ModuleController extends MVVController
                         $success_message,
                         htmlReady($this->modul->getDisplayName())
                     ));
-                    $this->redirect($this->indexURL());
+                    $this->redirect($this->indexURL(['modul_id' => $this->modul->id]) . "#modul_{$this->modul->id}");
                     return;
                 }
             }
