86a192c7fb30efdc5561a492ebdbc5da842781ed
Ron Lucke <lucke@elan-ev.de>
2024-08-16 07:22:04 +0000

    fix #1461
    Closes #1461
    
    Merge request studip/studip!3264

--- resources/vue/store/courseware/courseware.module.js
+++ resources/vue/store/courseware/courseware.module.js
@@ -1493,6 +1493,9 @@ export const actions = {
     toggleHideEditLayout({ commit, rootGetters}) {
         commit('setHideEditLayout', !rootGetters['hideEditLayout']);
     },
+    setHideEditLayout({commit}, state) {
+        commit('setHideEditLayout', state);
+    },
     setFeedbackSettings(context, feedbackSettings) {
         context.commit('setFeedbackSettings', feedbackSettings);
     },
