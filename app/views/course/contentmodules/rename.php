f0cb4a441937c5c84b032cccd2a16bc9b9e0472e
Jan-Hendrik Willms <tleilax+studip@gmail.com>
2024-08-19 06:26:53 +0000

    correctly display original name, fixes #4419
    Closes #4419
    
    Merge request studip/studip!3277

--- app/views/course/contentmodules/rename.php
+++ app/views/course/contentmodules/rename.php
@@ -1,3 +1,12 @@
+<?php
+/**
+ * @var Course_ContentmodulesController $controller
+ * @var StudipModule $module
+ * @var string $original_name
+ * @var array $metadata
+ * @var ToolActivation $tool
+ */
+?>
 <form class="default"
       action="<?= $controller->link_for('course/contentmodules/rename/' . $module->getPluginId()) ?>"
       method="post">
@@ -7,17 +16,17 @@
             <?= _('Neuer Name des Werkzeugs') ?>
             <input type="text"
                    name="displayname"
-                   value="<?= $tool && $tool['metadata'] ? htmlReady($tool['metadata']['displayname']) : ''?>"
-                   placeholder="<?= htmlReady($metadata['displayname'] ?? '') ?>">
+                   value="<?= htmlReady($tool['metadata']['displayname'] ?? '') ?>"
+                   placeholder="<?= htmlReady($original_name) ?>">
         </label>
 
         <div>
-            <?= htmlReady(sprintf(_('Ursprünglicher Werkzeugname ist "%s".'), $metadata['displayname'] ?? '')) ?>
+            <?= htmlReady(sprintf(_('Ursprünglicher Werkzeugname ist "%s".'), $original_name)) ?>
         </div>
     </fieldset>
     <div data-dialog-button>
         <?= \Studip\Button::create(_('Speichern'))?>
-        <? if ($tool && $tool['metadata'] && $tool['metadata']['displayname']) : ?>
+        <? if (!empty($tool['metadata']['displayname'])) : ?>
             <?= \Studip\Button::create(_('Namen löschen'), 'delete') ?>
         <? endif ?>
     </div>
