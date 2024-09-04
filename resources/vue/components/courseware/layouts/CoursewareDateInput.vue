6e3ca2f07b3c7770500c6884ce6d879c3ac0c57f
Jan-Hendrik Willms <tleilax+github@gmail.com>
2024-09-04 11:01:09 +0200

    Revert "Courseware: Datum bei Verlängerung von Aufgaben"
    This reverts commit 226ce75259541dc2e62823a037699d057a090297.

--- resources/vue/components/courseware/layouts/CoursewareDateInput.vue
+++ resources/vue/components/courseware/layouts/CoursewareDateInput.vue
@@ -1,5 +1,5 @@
 <template>
-    <input :value="formattedDate" @input="onInput" type="date" :min="formattedMinDate" />
+    <input :value="formattedDate" @input="onInput" type="date" />
 </template>
 
 <script>
@@ -8,18 +8,14 @@ const toISO8601 = (date) => date.toISOString();
 const pad = (what, length = 2) => `00000000${what}`.substr(-length);
 
 export default {
-    props: ['value', 'min'],
+    props: ['value'],
     data: () => ({
         date: new Date(),
-        submissionDate: new Date()
     }),
     computed: {
         formattedDate() {
             return `${this.date.getFullYear()}-${pad(this.date.getMonth() + 1)}-${pad(this.date.getDate())}`;
         },
-        formattedMinDate() {
-            return `${this.submissionDate.getFullYear()}-${pad(this.submissionDate.getMonth() + 1)}-${pad(this.submissionDate.getDate())}`;
-        }
     },
     methods: {
         onInput({ target }) {
@@ -32,9 +28,6 @@ export default {
     beforeMount() {
         if (this.value) {
             this.date = fromISO8601(this.value);
-        }
-        if (this.min) {
-            this.submissionDate = fromISO8601(this.min);
         }
     },
 };
