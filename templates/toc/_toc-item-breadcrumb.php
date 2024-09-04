b6fabdc999c83ed457d3b812bc90eaf587a357d0
Jan-Hendrik Willms <tleilax+studip@gmail.com>
2024-09-04 08:26:58 +0000

    fix alignment and reduction of breadcrumbs in content bar, fixes #4503
    Closes #4503
    
    Merge request studip/studip!3292

--- templates/toc/_toc-item-breadcrumb.php
+++ templates/toc/_toc-item-breadcrumb.php
@@ -1,7 +1,7 @@
 <? if (!$item->isRoot()) : ?>
     <?= $this->render_partial('toc/_toc-item-breadcrumb', ['item' => $item->getParent()]) ?>
 <? endif ?>
-<li>
+<li class="contentbar-breadcrumb-item <? if ($item->isActive()) echo 'contentbar-breadcrumb-item-current'; ?>">
     <? if (!$item->isActive()) : ?>
         <a class="navigate" href="<?= htmlReady($item->getURL()) ?>">
     <? endif ?>
