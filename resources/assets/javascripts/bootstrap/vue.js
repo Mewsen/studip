5cf39315d2a56cd736a7915eb1099776a2094c8c
Jan-Hendrik Willms <tleilax+github@gmail.com>
2024-09-23 15:48:53 +0200

    satisfy eslint, re #3747

--- resources/assets/javascripts/bootstrap/vue.js
+++ resources/assets/javascripts/bootstrap/vue.js
@@ -1,7 +1,37 @@
 import { defineAsyncComponent } from 'vue';
 
+function attachComponents(app, configuredComponents) {
+    configuredComponents.forEach(component => {
+        const name = component.split('/').reverse()[0];
+        app.component(name, defineAsyncComponent(() => {
+            const temp = import(`../../../vue/components/${component}.vue`);
+            temp.then(({default: c}) => {
+                const mounted = c.mounted ?? null;
+                c.mounted = function (...args) {
+                    if (
+                        this.$el instanceof Element
+                        && this.$el.closest('.studip-dialog')
+                        && this.$el.querySelector('[data-dialog-button]')
+                    ) {
+                        this.$el.closest('.studip-dialog')
+                            .querySelector('.ui-dialog-buttonpane')
+                            .remove();
+                    }
+                    if (mounted) {
+                        mounted.call(this, args);
+                    }
+                };
+                return c;
+            })
+            return temp;
+        }));
+    });
+}
+
 STUDIP.ready(() => {
-    document.querySelectorAll('[data-vue-app]:not([data-vue-app-created])').forEach((node) => {
+    document.querySelectorAll('[data-vue-app]:not([data-vue-app-created])').forEach(async (node) => {
+        node.dataset.vueAppCreated = 'true';
+
         const config = Object.assign(
             {
                 components: [],
@@ -11,99 +41,70 @@ STUDIP.ready(() => {
             JSON.parse(node.dataset.vueApp)
         );
 
-        let components = {};
-        config.components.forEach(component => {
-            const name = component.split('/').reverse()[0];
-            components[name] = defineAsyncComponent(() => {
-                // TODO: I wonder if this works with Vue3
+        const { createApp, store } = await STUDIP.Vue.load();
 
-                const temp = import(`../../../vue/components/${component}.vue`);
-                temp.then(({default: c}) => {
-                    const mounted = c.mounted ?? null;
-                    c.mounted = function (...args) {
-                        if (
-                            this.$el instanceof Element
-                            && this.$el.closest('.studip-dialog')
-                            && this.$el.querySelector('[data-dialog-button]')
-                        ) {
-                            this.$el.closest('.studip-dialog')
-                                .querySelector('.ui-dialog-buttonpane')
-                                .remove();
-                        }
-                        if (mounted) {
-                            mounted.call(this, args);
-                        }
-                    };
-                    return c;
+        const promises = [Promise.resolve()];
+
+        for (const [index, name] of Object.entries(config.stores)) {
+            promises.push(
+                import(`../../../vue/store/${name}.js`).then(storeConfig => {
+                    store.registerModule(index, storeConfig.default);
+
+                    const dataElement = document.getElementById(`vue-store-data-${index}`);
+                    if (dataElement) {
+                        const data = JSON.parse(dataElement.innerText);
+                        Object.keys(data).forEach(command => {
+                            store.commit(`${index}/${command}`, data[command]);
+                        });
+
+                        dataElement.remove();
+                    }
                 })
-                return temp;
-            });
+            );
+        }
+
+        const plugins = [];
+        for (const [plugin, filename] of Object.entries(config.plugins)) {
+            promises.push(
+                import(`../../../vue/plugins/${filename}.js`)
+                .then((temp) => plugins.push(temp[plugin]))
+            );
+        }
+
+        await Promise.all(promises);
+
+        const app = createApp({
+            store,
+
+            beforeCreate() {
+                STUDIP.Vue.emit('VueAppWillCreate', this);
+            },
+            created() {
+                STUDIP.Vue.emit('VueAppDidCreate', this);
+            },
+            beforeMount() {
+                STUDIP.Vue.emit('VueAppWillMount', this);
+            },
+            mounted() {
+                STUDIP.Vue.emit('VueAppDidMount', this);
+            },
+            beforeUpdate() {
+                STUDIP.Vue.emit('VueAppWillUpdate', this);
+            },
+            updated() {
+                STUDIP.Vue.emit('VueAppDidUpdate', this);
+            },
+            beforeUnmount() {
+                STUDIP.Vue.emit('VueAppWillUnmount', this);
+            },
+            unmounted() {
+                STUDIP.Vue.emit('VueAppDidUnmount', this);
+            },
         });
 
-        STUDIP.Vue.load().then(({createApp, store, Vue}) => {
-            const promises = [Promise.resolve()];
+        attachComponents(app, config.components);
+        plugins.forEach(plugin => app.use(plugin, { store }))
 
-            for (const [index, name] of Object.entries(config.stores)) {
-                promises.push(
-                    import(`../../../vue/store/${name}.js`).then(storeConfig => {
-                        store.registerModule(index, storeConfig.default);
-
-                        const dataElement = document.getElementById(`vue-store-data-${index}`);
-                        if (dataElement) {
-                            const data = JSON.parse(dataElement.innerText);
-                            Object.keys(data).forEach(command => {
-                                store.commit(`${index}/${command}`, data[command]);
-                            });
-
-                            dataElement.remove();
-                        }
-                    })
-                );
-            }
-
-            const plugins = [];
-            for (const [plugin, filename] of Object.entries(config.plugins)) {
-                promises.push(
-                    import(`../../../vue/plugins/${filename}.js`)
-                    .then((temp) => plugins.push(temp[plugin]))
-                );
-            }
-
-            Promise.all(promises).then(() => {
-                const app = createApp({
-                    components,
-                    store,
-
-                    beforeCreate() {
-                        STUDIP.Vue.emit('VueAppWillCreate', this);
-                    },
-                    created() {
-                        STUDIP.Vue.emit('VueAppDidCreate', this);
-                    },
-                    beforeMount() {
-                        STUDIP.Vue.emit('VueAppWillMount', this);
-                    },
-                    mounted() {
-                        STUDIP.Vue.emit('VueAppDidMount', this);
-                    },
-                    beforeUpdate() {
-                        STUDIP.Vue.emit('VueAppWillUpdate', this);
-                    },
-                    updated() {
-                        STUDIP.Vue.emit('VueAppDidUpdate', this);
-                    },
-                    beforeUnmount() {
-                        STUDIP.Vue.emit('VueAppWillUnmount', this);
-                    },
-                    unmounted() {
-                        STUDIP.Vue.emit('VueAppDidUnmount', this);
-                    },
-                });
-                plugins.forEach(plugin => app.use(plugin, { store }))
-                app.mount(node);
-            });
-        });
-
-        node.dataset.vueAppCreated = 'true';
+        app.mount(node);
     });
 });
