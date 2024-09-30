80bd1c1308ad63d1348fca78c51ab3220e080cb8
Jan-Hendrik Willms <tleilax+studip@gmail.com>
2024-09-30 09:16:38 +0000

    remove remains of flash player, fixes #4643
    Closes #4643
    
    Merge request studip/studip!3463

--- templates/shared/opengraphinfo_wide.php
+++ templates/shared/opengraphinfo_wide.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * @var OpenGraphURL $og
+ */
+?>
+<?php
 $videofiles = $og->getVideoFiles();
 $audiofiles = $og->getAudioFiles();
 $og['image'] = filter_var($og['image'], FILTER_VALIDATE_URL) ? $og['image'] : '';
@@ -32,38 +37,20 @@ if (Config::get()->LOAD_EXTERNAL_MEDIA === "proxy" && Seminar_Session::is_curren
     </a>
 <? if (count($videofiles)) : ?>
     <div class="video">
-    <? if (in_array($videofiles[0][1], ["text/html", "application/x-shockwave-flash"])) : ?>
-        <a href="<?= htmlReady($videofiles[0][0]) ?>"
-           class="flash-embedder"
-           style="background-image: url('<?= htmlReady($media_url_func($og['image'])) ?>');"
-           title="<?= _("Video abspielen") ?>">
-            <?= Icon::create('play', 'clickable')->asImg(80, ["class" => "play"])?>
-        </a>
-    <? else : ?>
         <video width="100%" height="200px" controls>
         <? foreach ($videofiles as $file) : ?>
-            <source src="<?= htmlReady($media_url_func($file[0])) ?>"<?= $file[1] ? ' type="'.htmlReady($file[1]).'"' : "" ?>></source>
+            <source src="<?= htmlReady($media_url_func($file[0])) ?>"<?= $file[1] ? ' type="'.htmlReady($file[1]).'"' : "" ?>>
         <? endforeach ?>
         </video>
-    <? endif ?>
     </div>
 <? endif ?>
 <? if (count($audiofiles)) : ?>
     <div class="audio">
-    <? if (in_array($audiofiles[0][1], ["text/html", "application/x-shockwave-flash"])) : ?>
-        <a href="<?= htmlReady($audiofiles[0][0]) ?>"
-           class="flash-embedder"
-           style="background-image: url('<?= htmlReady($media_url_func($og['image'])) ?>');"
-           title="<?= _("Audio abspielen") ?>">
-            <?= Icon::create('play', 'clickable')->asImg(100)?>
-        </a>
-    <? else : ?>
         <audio width="100%" height="50px" controls>
         <? foreach ($audiofiles as $file) : ?>
-            <source src="<?= htmlReady($media_url_func($file[0])) ?>"<?= $file[1] ? ' type="'.htmlReady($file[1]).'"' : "" ?>></source>
+            <source src="<?= htmlReady($media_url_func($file[0])) ?>"<?= $file[1] ? ' type="'.htmlReady($file[1]).'"' : "" ?>>
         <? endforeach ?>
         </audio>
-    <? endif ?>
     </div>
 <? endif ?>
 </div>
