3450a05d71786caee71186a4170da263788b763b
Rami Jasim <rami.jasim1@uni-oldenburg.de>
2024-10-14 12:17:02 +0200

    add performance details into the footer for debugging

--- templates/footer.php
+++ templates/footer.php
@@ -1,37 +1,60 @@
 <? if (PageLayout::isFooterEnabled()): ?>
-<!-- Beginn Footer -->
-<?= SkipLinks::addIndex(_('Fußzeile'), 'main-footer', 900, false) ?>
-<footer id="main-footer" aria-label="<?= _('Fußzeile') ?>">
-<? if (is_object($GLOBALS['user']) && $GLOBALS['user']->id != 'nobody') : ?>
-    <div id="main-footer-info">
-        <? printf(_('Sie sind angemeldet als %s (%s)'),
-                  htmlReady($GLOBALS['user']->username),
-                  htmlReady($GLOBALS['user']->perms)) ?>
-        |
-        <?= strftime('%x, %X') ?>
-    </div>
-<? endif; ?>
+    <!-- Beginn Footer -->
+    <?= SkipLinks::addIndex(_('Fußzeile'), 'main-footer', 900, false) ?>
+    <footer id="main-footer" aria-label="<?= _('Fußzeile') ?>">
+        <? if (is_object($GLOBALS['user']) && $GLOBALS['user']->id != 'nobody') : ?>
+            <div id="main-footer-info">
+                <? printf(_('Sie sind angemeldet als %s (%s)'),
+                    htmlReady($GLOBALS['user']->username),
+                    htmlReady($GLOBALS['user']->perms)) ?>
+                |
+                <?= strftime('%x, %X') ?>
+                <? if (Studip\ENV === 'development'): ?>
+                    [
+                    <? if (DBManager::get('studip') === DBManager::get('studip-slave')): ?>
+                        <?= sprintf('%u db queries', DBManager::get('studip')->query_count) ?>
+                    <? else: ?>
+                        <?= sprintf(
+                            'M%u/S%u = %u db queries',
+                            DBManager::get('studip')->query_count,
+                            DBManager::get('studip-slave')->query_count,
+                            DBManager::get('studip')->query_count + DBManager::get('studip-slave')->query_count
+                        ) ?>
+                    <? endif; ?>
+                    /
+                    <?= relsize(memory_get_peak_usage(true), false) ?> mem
+                    /
+                    <?= sprintf('%.5f sec', microtime(true) - $GLOBALS['STUDIP_STARTUP_TIME']) ?>
+                    ]
+                    <? if (!empty($GLOBALS['DEBUG_ALL_DB_QUERIES'])) : ?>
+                        <a href="" onClick="jQuery('#all_db_queries').toggle(); return false;">
+                            <?= Icon::create("code", "info_alt")->asImg(16, ['class' => "text-bottom"]) ?>
+                        </a>
+                    <? endif ?>
+                <? endif; ?>
+            </div>
+        <? endif; ?>
 
-<? if (Navigation::hasItem('/footer')) : ?>
-    <nav id="main-footer-navigation" aria-label="<?= _('Fußzeilennavigation') ?>">
-        <ul>
-        <? foreach (Navigation::getItem('/footer') as $nav): ?>
-            <? if ($nav->isVisible()): ?>
-                <li>
-                <a
-                <? if (is_internal_url($url = $nav->getURL())) : ?>
-                    href="<?= URLHelper::getLink($url, $link_params ?? null) ?>"
-                <? else: ?>
-                    href="<?= htmlReady($url) ?>" target="_blank" rel="noopener noreferrer"
-                <? endif ?>
-                    <?= arrayToHtmlAttributes($nav->getLinkAttributes()) ?>
-                ><?= htmlReady($nav->getTitle()) ?></a>
-                </li>
-            <? endif; ?>
-        <? endforeach; ?>
-        </ul>
-    </nav>
-<? endif; ?>
-</footer>
+        <? if (Navigation::hasItem('/footer')) : ?>
+            <nav id="main-footer-navigation" aria-label="<?= _('Fußzeilennavigation') ?>">
+                <ul>
+                    <? foreach (Navigation::getItem('/footer') as $nav): ?>
+                        <? if ($nav->isVisible()): ?>
+                            <li>
+                                <a
+                                    <? if (is_internal_url($url = $nav->getURL())) : ?>
+                                        href="<?= URLHelper::getLink($url, $link_params ?? null) ?>"
+                                    <? else: ?>
+                                        href="<?= htmlReady($url) ?>" target="_blank" rel="noopener noreferrer"
+                                    <? endif ?>
+                                    <?= arrayToHtmlAttributes($nav->getLinkAttributes()) ?>
+                                ><?= htmlReady($nav->getTitle()) ?></a>
+                            </li>
+                        <? endif; ?>
+                    <? endforeach; ?>
+                </ul>
+            </nav>
+        <? endif; ?>
+    </footer>
 <? endif; ?>
 <!-- Ende Footer -->
