46ac50ef369e0aa243d9eb87b2935ad9c6039c52
Jan-Hendrik Willms <tleilax+studip@gmail.com>
2024-11-13 16:47:50 +0100

    fix syntax error from f6e64b1e, re #4863

--- lib/models/resources/ResourceRequest.class.php
+++ lib/models/resources/ResourceRequest.class.php
@@ -1470,7 +1470,7 @@ class ResourceRequest extends SimpleORMap implements PrivacyObject, Studip\Calen
             $strings = $appointments->map(
                 function (ResourceRequestAppointment $appointment): string {
                     return $appointment->appointment->getFullName('include-room');
-                },
+                }
             );
         } elseif ($this->termin_id) {
             if ($this->date) {
