6c14b24a0ff71f26bea19c827860220371e5eccf
Jan-Hendrik Willms <tleilax+studip@gmail.com>
2024-12-18 09:08:32 +0000

    set attribute PDO::ATTR_STRINGIFY_FETCHES in production mode, fixes #4971
    Closes #4971
    
    Merge request studip/studip!3732

--- lib/bootstrap.php
+++ lib/bootstrap.php
@@ -159,6 +159,11 @@ if (isset($GLOBALS['DB_STUDIP_SLAVE_HOST'])) {
     DBManager::getInstance()->aliasConnection('studip', 'studip-slave');
 }
 
+if (Studip\ENV === 'production') {
+    DBManager::get()->setAttribute(PDO::ATTR_STRINGIFY_FETCHES, true);
+    DBManager::get('studip-slave')->setAttribute(PDO::ATTR_STRINGIFY_FETCHES, true);
+}
+
 // set default exception handler
 // command line or http request?
 if (isset($_SERVER['REQUEST_METHOD'])) {
