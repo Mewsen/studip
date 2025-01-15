021f9b855ecacbbcc040bfbfce8f6cce6da9b5c1
Jan-Hendrik Willms <tleilax+studip@gmail.com>
2025-01-15 15:43:01 +0000

    remove whitespace and add php docblock, fixes #5128
    Closes #5128
    
    Merge request studip/studip!3833

--- templates/mail/text.php
+++ templates/mail/text.php
@@ -1,11 +1,18 @@
-<?
-# Lifter010: TODO
+<?php
+/**
+ * @var string $message
+ * @var FileRef[]|null $attachments
+ * @var string $snd_fullname
+ * @var string $snd_email
+ * @var string $rec_fullname
+ * @var string $rec_email
+ */
 ?>
 <?= $message ?>
 
 <? if (isset($attachments) && count($attachments)) : ?>
 
-    <?= _("Dateianhänge:") ?>
+    <?= _('Dateianhänge:') ?>
 
     <? foreach ($attachments as $attachment) : ?>
         <?= $attachment->name . ' (' . relsize($attachment->file->size, false) . ')' ?>
@@ -16,11 +23,10 @@
     <? endforeach; ?>
 <? endif; ?>
 
-
 --
 <? if ($snd_fullname) : ?>
-    <?= sprintf(_('Diese E-Mail ist eine Kopie einer systeminternen Nachricht, die in Stud.IP von %s (%s) an %s (%s) versendet wurde.'), $snd_fullname, $snd_email, $rec_fullname, $rec_email) ?>
+<?= sprintf(_('Diese E-Mail ist eine Kopie einer systeminternen Nachricht, die in Stud.IP von %s (%s) an %s (%s) versendet wurde.'), $snd_fullname, $snd_email, $rec_fullname, $rec_email) ?>
 <? else : ?>
-    <?= sprintf(_("Diese E-Mail ist eine Kopie einer systeminternen Nachricht, die in Stud.IP an %s versendet wurde."), $rec_fullname) ?>
+<?= sprintf(_('Diese E-Mail ist eine Kopie einer systeminternen Nachricht, die in Stud.IP an %s versendet wurde.'), $rec_fullname) ?>
 <? endif ?>
-<?= sprintf(_("Sie erreichen Stud.IP unter %s"), $GLOBALS['ABSOLUTE_URI_STUDIP']) ?>
+<?= sprintf(_('Sie erreichen Stud.IP unter %s'), $GLOBALS['ABSOLUTE_URI_STUDIP']) ?>
