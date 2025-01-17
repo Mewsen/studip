ddce12c92465e45f4456992990a5109c3c3f588e
Ron Lucke <lucke@elan-ev.de>
2025-01-17 14:15:09 +0000

    Courseware: Beim Importieren wird zu jeder Seite ein leerer Abschnitt hinzugefügt
    Closes #5139
    
    Merge request studip/studip!3850

--- resources/vue/mixins/courseware/import.js
+++ resources/vue/mixins/courseware/import.js
@@ -172,6 +172,7 @@ export default {
                             attributes: element[i].attributes,
                             parentId: parent_id,
                             currentId: parent_id,
+                            withDefaultContainer: false
                         });
                         new_element = this.lastCreatedElements;
                     } catch {
