056f558ca97e955bc8739e30c97ea30b01e60348
Ron Lucke <lucke@elan-ev.de>
2025-01-22 19:02:50 +0000

    PolishingTIC: Sichtbarkeit des Erstellen von Ankündigungen auf der Loginseite
    Closes #4956
    
    Merge request studip/studip!3854

--- lib/classes/forms/NewsRangesInput.php
+++ lib/classes/forms/NewsRangesInput.php
@@ -28,12 +28,21 @@ class NewsRangesInput extends Input
 
         $selectable = [];
 
-        // Stud.IP
+        // System
         $studip_options = $this->getStudipOptions();
         if (count($studip_options) > 0) {
             $selectable[] = [
-                'label' => _('Stud.IP'),
+                'label' => _('System'),
                 'options' => $studip_options
+            ];
+        }
+
+        // User
+        $user_options = $this->getUserOptions();
+        if (count($user_options) > 0) {
+            $selectable[] = [
+                'label' => _('Personen'),
+                'options' => $user_options
             ];
         }
 
@@ -122,6 +131,14 @@ class NewsRangesInput extends Input
             ];
         }
 
+
+        return $options;
+    }
+
+    public function getUserOptions(): array
+    {
+        $options = [];
+        
         $options[] = [
             'value' => \User::findCurrent()->id . '__person',
             'name' => _('Meine Profilseite')
