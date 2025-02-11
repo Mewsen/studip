4a99127c3cdced499bf4d73b3efa47b155c6cc65
Jan-Hendrik Willms <tleilax+studip@gmail.com>
2025-02-11 16:58:32 +0100

    add aria tooltip role to tooltips, fixes #5238

--- templates/shared/tooltip.php
+++ templates/shared/tooltip.php
@@ -3,13 +3,15 @@
  * @var bool $important
  * @var bool $html
  * @var string $text
+ * @var bool $alt_info
  */
 ?>
-<span 
+<span
     class="as-link tooltip tooltip-icon
-        <? if ($important) echo 'tooltip-important'; ?> 
+        <? if ($important) echo 'tooltip-important'; ?>
         <? if ($alt_info)  echo 'tooltip-info-alt'; ?>"
     tabindex="0"
+    role="tooltip"
     data-tooltip
     aria-label="<?= htmlReady($html ? strip_tags($text) : $text) ?>"
 >
