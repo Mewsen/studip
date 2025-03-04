a800085a3b13af5781ec59a62e729dff4737a57c
Ron Lucke <lucke@elan-ev.de>
2025-03-04 14:00:50 +0000

    MultiPersonSerach: Dialog vertikal etwas zu klein
    Closes #5228
    
    Merge request studip/studip!4004

--- templates/multipersonsearch/link.php
+++ templates/multipersonsearch/link.php
@@ -1,4 +1,4 @@
-<a href="<?= URLHelper::getLink('dispatch.php/multipersonsearch/no_js_form/?name=' . $name); ?>" class="multi_person_search_link" data-dialog="width=720;height=460;id=mp-search"  data-dialogname="<?= $name; ?>" title="<?= htmlReady($title) ?>" data-js-form="<?= URLHelper::getLink('dispatch.php/multipersonsearch/js_form/' . $name); ?>">
+<a href="<?= URLHelper::getLink('dispatch.php/multipersonsearch/no_js_form/?name=' . $name); ?>" class="multi_person_search_link" data-dialog="width=720;height=490;id=mp-search"  data-dialogname="<?= $name; ?>" title="<?= htmlReady($title) ?>" data-js-form="<?= URLHelper::getLink('dispatch.php/multipersonsearch/js_form/' . $name); ?>">
     <?
     if (!empty($linkIconPath)) {
         print $linkIconPath->asImg(['class' => 'action-menu-item-icon']);
