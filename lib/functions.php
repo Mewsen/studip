f4db0ff9d87a296693d0a1bba76292359d6f8103
Philipp Schüttlöffel <schuettloeffel@zqs.uni-hannover.de>
2025-03-20 07:46:22 +0000

    Resolve "Probleme mit get_default_http_stream_context() und Proxy"
    Closes #5388
    
    Merge request studip/studip!4054

--- lib/functions.php
+++ lib/functions.php
@@ -1477,10 +1477,10 @@ function get_default_http_stream_context($url = '')
     }
     if ($proxy) {
         $opts = ['http' => ['proxy' => 'tcp://' . $proxy]];
+        return stream_context_create($opts);
     } else {
-        $opts = [];
+        return stream_context_get_default();
     }
-    return stream_context_get_default($opts);
 }
 
 /**
