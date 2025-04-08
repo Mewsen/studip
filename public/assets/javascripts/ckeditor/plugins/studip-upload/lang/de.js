28df968b5b4cf9bdadc582b536795b0ef0ee34a1
Marcus Eibrink-Lunzenauer <lunzenauer@elan-ev.de>
2025-04-08 11:19:22 +0000

    Remove CKE v4.
    Closes #5473
    
    Merge request studip/studip!4110

--- public/assets/javascripts/ckeditor/plugins/studip-upload/lang/de.js
+++
@@ -1,7 +0,0 @@
-CKEDITOR.plugins.setLang('studip-upload', 'de', {
-    buttonLabel: 'Datei hochladen',
-    error: 'Fehler: ',
-    uploadError: 'Es konnten nicht alle Dateien hochgeladen werden.',
-    uploadFailed: 'Das Hochladen der Datei(en) ist fehlgeschlagen.',
-});
-
