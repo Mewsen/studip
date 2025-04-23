e7f7991addc013112aa976cc444da53ad7514774
Jan-Hendrik Willms <tleilax+studip@gmail.com>
2025-04-23 10:29:35 +0000

    set localized date format for jquery ui datepicker, fixes #5507
    Closes #5507
    
    Merge request studip/studip!4135

--- resources/assets/javascripts/studip-ui.js
+++ resources/assets/javascripts/studip-ui.js
@@ -673,7 +673,7 @@ import eventBus from "./lib/event-bus.ts";
                 $gettext('Sa')
             ],
             weekHeader: $gettext('Wo'),
-            dateFormat: 'dd.mm.yy',
+            dateFormat: String.locale === 'en-GB' ? 'mm/dd/yy' : 'dd.mm.yy',
             firstDay: 1,
             isRTL: false,
             showMonthAfterYear: false,
