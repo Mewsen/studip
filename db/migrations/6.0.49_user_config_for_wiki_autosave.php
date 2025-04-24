414412f65c4c020541d0ad0b7e40e728f3cd5724
Jan-Hendrik Willms <tleilax+studip@gmail.com>
2025-04-24 16:47:16 +0200

    add user configuration to enable wiki autosave feature, fixes #5345
    Closes #5345
    
    Merge request studip/studip!4031

---
+++ db/migrations/6.0.49_user_config_for_wiki_autosave.php
@@ -0,0 +1,31 @@
+<?php
+final class UserConfigForWikiAutosave extends Migration
+{
+    public function description()
+    {
+        return 'Adds a user configuration for autostoring of wiki pages';
+    }
+
+    public function up()
+    {
+        $query = "INSERT IGNORE INTO `config` (
+                    `field`, `value`, `type`, `range`, `section`,
+                    `mkdate`, `chdate`,
+                    `description`
+                  ) VALUES (
+                    'WIKI_ENABLE_AUTOSAVE', '0', 'boolean', 'user', 'wiki',
+                    UNIX_TIMESTAMP(), UNIX_TIMESTAMP(),
+                    'Aktiviert das automatische Speichern im Wiki'
+                  )";
+        DBManager::get()->exec($query);
+    }
+
+    public function down()
+    {
+        $query = "DELETE `config`, `config_values`
+                  FROM `config`
+                  LEFT JOIN `config_values` USING (`field`)
+                  WHERE `field` = 'WIKI_ENABLE_AUTOSAVE'";
+        DBManager::get()->exec($query);
+    }
+}
