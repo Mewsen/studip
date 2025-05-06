bc30111d8dbff3e8d9b030f9974b6b4c7c21b8e9
Ron Lucke <lucke@elan-ev.de>
2025-05-06 10:10:37 +0200

    Unsichtbare Courseware-Seiten sind erreichbar aber leer
    Merge request studip/studip!4202

--- resources/vue/components/courseware/IndexApp.vue
+++ resources/vue/components/courseware/IndexApp.vue
@@ -34,7 +34,15 @@
             v-if="structureLoadingState === 'error'"
             mood="sad"
             :msgCompanion="loadingErrorMessage"
-        />
+        >
+         <template #companionActions>
+                <a :href="rootElementUrl">
+                    <button class="button">
+                        {{ $gettext('Erste Seite aufrufen') }}
+                    </button>
+                </a>
+            </template>
+        </courseware-companion-box>
         <courseware-companion-overlay />
     </div>
 </template>
@@ -83,7 +91,8 @@ export default {
             structuralElements: 'courseware-structural-elements/all',
             structuralElementById: 'courseware-structural-elements/byId',
             userId: 'userId',
-            userIsTeacher: 'userIsTeacher'
+            userIsTeacher: 'userIsTeacher',
+            instanceById: 'courseware-instances/byId',
         }),
         loadingErrorMessage() {
             switch (this.loadingErrorStatus) {
@@ -101,6 +110,26 @@ export default {
             }
 
             return false;
+        },
+        currentInstance() {
+            return this.instanceById({ id: `course_${this.context.id}_${this.context.unit}`});
+        },
+        rootElementId() {
+           return this.currentInstance.relationships.root.data.id;
+        },
+        rootElementUrl() {
+            if (this.context.type === 'users') {
+                return STUDIP.URLHelper.getURL(
+                    `dispatch.php/contents/courseware/courseware/${this.context.unit }/${this.context.id}#/structural_element/${this.rootElementId}`
+                );
+            }
+            if (this.context.type === 'courses') {
+                return STUDIP.URLHelper.getURL(
+                    `dispatch.php/course/courseware/courseware/${this.context.unit }/${this.context.id}#/structural_element/${this.rootElementId}`
+                );
+            }
+
+            return '';
         }
     },
     methods: {
