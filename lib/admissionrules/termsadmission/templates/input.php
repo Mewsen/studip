eb8cf14945ba2224eed4fd04957f5f0fdce4d9fd
Thomas Hackl <hackl@data-quest.de>
2025-06-18 13:54:30 +0200

    Resolve "Fehlerhafte Darstellung bei der Anmelderegel: "Kurs mit Teilnahmebedingungen""
    Closes #5427
    
    Merge request studip/studip!4261

--- lib/admissionrules/termsadmission/templates/input.php
+++ lib/admissionrules/termsadmission/templates/input.php
@@ -1,9 +1,5 @@
 <?= htmlReady($rule->getName()) ?>
-
-<div class="messagebox_details">
-    <?= formatLinks($rule->terms) ?>
-</div>
-
+<br><br>
 <label>
     <input type="checkbox" name="terms_accepted" value="1">
     <?= _('Hiermit akzeptiere ich die Teilnahmebedingungen') ?>
