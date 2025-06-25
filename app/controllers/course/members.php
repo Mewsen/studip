9ebab96c853d1d9695e528509f15d0aacfa2009c
Jan-Hendrik Willms <tleilax+studip@gmail.com>
2025-06-25 22:20:35 +0200

    verify aux is set before using it, fixes #5657
    Closes #5657
    
    Merge request studip/studip!4280

--- app/controllers/course/members.php
+++ app/controllers/course/members.php
@@ -1730,7 +1730,7 @@ class Course_MembersController extends AuthenticatedController
         ];
 
         $course = Course::findCurrent();
-        $aux     = $course->aux->getCourseData($course, true);
+        $aux = $course->aux ? $course->aux->getCourseData($course, true) : false;
 
         $members = $course->getMembersData($status);
 
