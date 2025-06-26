56812c9c775cd2179c58e8d304f093ca9770e8a1
Michaela Brückner <brueckner@data-quest.de>
2025-06-26 14:34:50 +0200

    Resolve "Globales Deaktivieren des Wiki: Werkzeug kann dennoch ausgewählt werden"
    Closes #5668
    
    Merge request studip/studip!4289

--- lib/modules/CoreWiki.class.php
+++ lib/modules/CoreWiki.class.php
@@ -204,4 +204,9 @@ class CoreWiki extends CorePlugin implements StudipModule
         return $root;
     }
 
+    public function isActivatableForContext(Range $context)
+    {
+        return (bool) Config::get()->getValue('WIKI_ENABLE');
+    }
+
 }
