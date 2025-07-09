b27cdf0733b9651ae05d45c7215875f8f96f440c
Ron Lucke <lucke@elan-ev.de>
2025-07-09 19:50:55 +0200

    Courseware: Anklicken eines geteilten Lernmaterials produziert Fehler
    Closes #5465
    
    Merge request studip/studip!4327

--- resources/vue/components/courseware/unit/CoursewareSharedItems.vue
+++ resources/vue/components/courseware/unit/CoursewareSharedItems.vue
@@ -2,45 +2,37 @@
     <div class="cw-shared-items">
         <h2 v-if="sharedElements.length > 0">{{ $gettext('Geteilte Lernmaterialien') }}</h2>
         <ul class="cw-tiles">
-            <li
+            <CoursewareTile
                 v-for="element in sharedElements"
                 :key="element.id"
-                class="tile"
-                :class="[element.attributes.payload.color, sharedElements.length > 3 ? '':  'cw-tile-margin']"
+                tag="li"
+                :color="element.attributes.payload.color"
+                :title="element.attributes.title"
+                :descriptionLink="getSharedElementUrl(element.id)"
+                :descriptionTitle="$gettext('Lernmaterial öffnen')"
+                :displayProgress="false"
+                :progress="0"
+                :imageUrl="element.relationships.image?.meta?.['download-url']"
             >
-                <a :href="getSharedElementUrl(element.id)" :title="element.attributes.title">
-                    <div
-                        class="preview-image"
-                        :class="[hasImage(element) ? '' : 'default-image']"
-                        :style="getChildStyle(element)"
-                    >
-                        <div class="overlay-text">{{ getOwnerName(element) }}</div>
-                    </div>
-                    <div class="description">
-                        <header
-                            :class="[element.attributes.purpose !== '' ? 'description-icon-' + element.attributes.purpose : '']"
-                        >
-                            {{ element.attributes.title }}
-                        </header>
-                        <div class="description-text-wrapper">
-                            <p>{{ element.attributes.payload.description }}</p>
-                        </div>
-                        <footer>
-                            {{ countChildren(element) + 1 }}
-                            {{ $ngettext('Seite', 'Seiten', countChildren(element) + 1) }}
-                        </footer>
-                    </div>
-                </a>
-            </li>
+                <template #image-overlay>
+                    {{ getOwnerName(element) }}
+                </template>
+                <template #description>
+                    {{ element.attributes.payload.description }}
+                </template>
+            </CoursewareTile>
         </ul>
     </div>
 </template>
 <script>
 import { mapGetters } from 'vuex';
-import {$ngettext} from "../../../../assets/javascripts/lib/gettext";
+import CoursewareTile from '../layouts/CoursewareTile.vue';
 
 export default {
     name: 'courseware-shared-items',
+    components: {
+        CoursewareTile,
+    },
     computed: {
         ...mapGetters({
             sharedElements: 'courseware-structural-elements-shared/all',
@@ -48,24 +40,19 @@ export default {
         }),
     },
     methods: {
-        $ngettext,
         getChildStyle(child) {
             let url = child.relationships?.image?.meta?.['download-url'];
 
-            if(url) {
-                return {'background-image': 'url(' + url + ')'};
+            if (url) {
+                return { 'background-image': 'url(' + url + ')' };
             } else {
                 return {};
             }
         },
-        hasImage(child) {
-            return child.relationships?.image?.data !== null;
-        },
-        getElementUrl(elementId) {
-            return STUDIP.URLHelper.base_url + 'dispatch.php/contents/courseware/courseware#/structural_element/' + elementId;
-        },
         getSharedElementUrl(elementId) {
-            return STUDIP.URLHelper.base_url + 'dispatch.php/contents/courseware/shared_content_courseware/' + elementId;
+            return (
+                STUDIP.URLHelper.base_url + 'dispatch.php/contents/courseware/shared_content_courseware/' + elementId
+            );
         },
         getOwnerName(element) {
             const ownerId = element.relationships.owner.data.id;
@@ -73,13 +60,6 @@ export default {
 
             return owner.attributes['formatted-name'];
         },
-        countChildren(element) {
-            let data = element.relationships.children.data;
-            if (data) {
-                return data.length;
-            }
-            return 0;
-        },
     },
-}
+};
 </script>
