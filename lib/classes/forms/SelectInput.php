73a016f48fa1882b5513f35ac729f7b22dc957ae
Rasmus Fuhse <fuhse@data-quest.de>
2025-07-10 07:50:05 +0000

    Resolve "Wiki: Vorbelegung "keine" bei "Übergeordnete Seite" funktioniert nicht"
    Closes #5277
    
    Merge request studip/studip!4336

--- lib/classes/forms/SelectInput.php
+++ lib/classes/forms/SelectInput.php
@@ -11,7 +11,7 @@ class SelectInput extends Input
         $template = $GLOBALS['template_factory']->open('forms/select_input');
         $template->title = $this->title;
         $template->name = $this->name;
-        $template->value = $this->value;
+        $template->value = (string) $this->value;
         $template->id = md5(uniqid());
         $template->required = $this->required;
         $template->attributes = arrayToHtmlAttributes($this->attributes);
