bae8ee2368bbb47f16d704623502bf5bdd2d1f97
Moritz Strohm <strohm@data-quest.de>
2025-07-23 10:15:42 +0000

    send delete notification for reservations and lock bookings, too, fixes #4749
    Closes #4749
    
    Merge request studip/studip!4376
    
    
    (cherry picked from commit 8676ca591aae20e87053e22fd40be680c8a3cc36)
    
    65a84077 send delete notification for reservations and lock bookings, too
    
    Co-authored-by: Moritz Strohm <strohm@data-quest.de>

--- locale/en/LC_MAILS/delete_booking_notification.php
+++ locale/en/LC_MAILS/delete_booking_notification.php
@@ -1,3 +1,4 @@
+<? if (in_array($booking_type, [ResourceBooking::TYPE_NORMAL, ResourceBooking::TYPE_PLANNED])) : ?>
 <? if ($resource instanceof Room): ?>
 Your booking of the room <?= $resource->name ?> on <?= date('d.m.Y', $begin) ?>
  from <?= date('H:i', $begin) ?> to <?= date('H:i', $end) ?> has been deleted.
@@ -5,9 +6,32 @@ Your booking of the room <?= $resource->name ?> on <?= date('d.m.Y', $begin) ?>
 Your booking of the resource <?= $resource->name ?> on <?= date('d.m.Y', $begin) ?>
  from <?= date('H:i', $begin) ?> to <?= date('H:i', $end) ?> has been deleted.
 <? endif ?>
+<? elseif ($booking_type == ResourceBooking::TYPE_RESERVATION) : ?>
+<? if ($resource instanceof Room): ?>
+Your reservation of the room <?= $resource->name ?> on <?= date('d.m.Y', $begin) ?>
+from <?= date('H:i', $begin) ?> to <?= date('H:i', $end) ?> has been deleted.
+<? else: ?>
+Your reservation of the resource <?= $resource->name ?> on <?= date('d.m.Y', $begin) ?>
+from <?= date('H:i', $begin) ?> to <?= date('H:i', $end) ?> has been deleted.
+<? endif ?>
+<? elseif ($booking_type == ResourceBooking::TYPE_LOCK) : ?>
+<? if ($resource instanceof Room): ?>
+Your lock booking of the room <?= $resource->name ?> on <?= date('d.m.Y', $begin) ?>
+from <?= date('H:i', $begin) ?> to <?= date('H:i', $end) ?> has been deleted.
+<? else: ?>
+Your lock booking of the resource <?= $resource->name ?> on <?= date('d.m.Y', $begin) ?>
+from <?= date('H:i', $begin) ?> to <?= date('H:i', $end) ?> has been deleted.
+<? endif ?>
+<? endif ?>
 
 <? if ($booking_course instanceof Course): ?>
-The booking deleted belonged to course <?= $booking_course->getFullName() ?>.
+<? if (in_array($booking_type, [ResourceBooking::TYPE_NORMAL, ResourceBooking::TYPE_PLANNED])) : ?>
+The deleted booking belonged to course <?= $booking_course->getFullName() ?>.
+<? elseif ($booking_type == ResourceBooking::TYPE_RESERVATION) : ?>
+The deleted reservation belonged to course <?= $booking_course->getFullName() ?>.
+<? elseif ($booking_type == ResourceBooking::TYPE_LOCK) : ?>
+The deleted lock booking belonged to course <?= $booking_course->getFullName() ?>.
+<? endif ?>
 <? endif ?>
 
 <? if ($deleting_user instanceof User) : ?>
