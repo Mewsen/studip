bd98b97ad60e4a8e899be11127eab5b9bae8a5cc
Thomas Hackl <hackl@data-quest.de>
2025-08-06 12:19:07 +0200

    some error handling on unavailable ZIP file

--- db/migrations/6.1.16_import_default_stock_images.php
+++ db/migrations/6.1.16_import_default_stock_images.php
@@ -14,75 +14,81 @@ class ImportDefaultStockImages extends Migration
         $extractPath = $GLOBALS['TMP_PATH'] . '/pool/';
 
         // Fetch zip with pool images.
-        file_put_contents(
-            $zipFile,
-            file_get_contents('https://gitlab.studip.de/studip/bilderpool/-/raw/main/Stud.IP_5_Bilderpool.zip?ref_type=heads&inline=false')
-        );
+        $content = @file_get_contents('https://gitlab.studip.de/studip/bilderpool/-/raw/main/Stud.IP_5_Bilderpool.zip?ref_type=heads&inline=false');
 
-        // Unzip archive to temp directory.
-        $zip = new ZipArchive();
-        $res = $zip->open($zipFile);
-        if ($res) {
-            $zip->extractTo($extractPath);
-            $zip->close();
-        } else {
-            unlink($zipFile);
-            die('Could not open zip file.');
-        }
+        if ($content !== false) {
 
-        // Get metadata and create stock images if not already present.
-        $csv_file = file($extractPath . 'meta.csv');
-        if (!$csv_file) {
+            file_put_contents($zipFile, $content);
+
+            // Unzip archive to temp directory.
+            $zip = new ZipArchive();
+            $res = $zip->open($zipFile);
+            if ($res) {
+                $zip->extractTo($extractPath);
+                $zip->close();
+            } else {
+                unlink($zipFile);
+                die('Could not open zip file.');
+            }
+
+            // Get metadata and create stock images if not already present.
+            $csv_file = file($extractPath . 'meta.csv');
+            if (!$csv_file) {
+                $this->cleanup($extractPath);
+                unlink($zipFile);
+                die('No meta.csv file provided.');
+            }
+
+            $rows = array_map(
+                fn($v) => str_getcsv($v, ';'),
+                $csv_file
+            );
+            $header = array_shift($rows);
+
+            $images = [];
+            foreach ($rows as $row) {
+                $images[] = array_combine($header, $row);
+            }
+
+            foreach ($images as $meta) {
+                $filename = $meta['filename'];
+                if (!$filename) {
+                    continue;
+                }
+
+                // Import file only if it doesn't already exist
+                if (!StockImage::findOneByDescription($meta['description'] ?? 'STOCKIMAGE')) {
+                    $filepath = $extractPath . $filename;
+                    $filesize = filesize($filepath);
+                    $imagesize = getimagesize($filepath);
+
+                    $image = \StockImage::create([
+                        'title' => $meta['title'] ?? 'unknown',
+                        'description' => $meta['description'] ?? '',
+                        'license' => $meta['license'] ?? '',
+                        'author' => $meta['author'] ?? '',
+                        'height' => $imagesize[1],
+                        'width' => $imagesize[0],
+                        'mime_type' => $imagesize['mime'],
+                        'size' => $filesize,
+                        'tags' => json_encode(explode(',', $meta['tags'])),
+                    ]);
+
+                    copy($filepath, $image->getPath());
+                    $scaler = new \Studip\StockImages\Scaler();
+                    $scaler($image);
+                    $paletteCreator = new \Studip\StockImages\PaletteCreator();
+                    $paletteCreator($image);
+                }
+            }
+
             $this->cleanup($extractPath);
             unlink($zipFile);
-            die('No meta.csv file provided.');
+
+        // No ZIP file found or file not accessible, just show an error message.
+        } else {
+            echo 'Could not open zip file.';
         }
-
-        $rows = array_map(
-            fn($v) => str_getcsv($v, ';'),
-            $csv_file
-        );
-        $header = array_shift($rows);
-
-        $images = [];
-        foreach ($rows as $row) {
-            $images[] = array_combine($header, $row);
-        }
-
-        foreach ($images as $meta) {
-            $filename = $meta['filename'];
-            if (!$filename) {
-                continue;
-            }
-
-            // Import file only if it doesn't already exist
-            if (!StockImage::findOneByDescription($meta['description'] ?? 'STOCKIMAGE')) {
-                $filepath = $extractPath . $filename;
-                $filesize = filesize($filepath);
-                $imagesize = getimagesize($filepath);
-
-                $image = \StockImage::create([
-                    'title' => $meta['title'] ?? 'unknown',
-                    'description' => $meta['description'] ?? '',
-                    'license' => $meta['license'] ?? '',
-                    'author' => $meta['author'] ?? '',
-                    'height' => $imagesize[1],
-                    'width' => $imagesize[0],
-                    'mime_type' => $imagesize['mime'],
-                    'size' => $filesize,
-                    'tags' => json_encode(explode(',', $meta['tags'])),
-                ]);
-
-                copy($filepath, $image->getPath());
-                $scaler = new \Studip\StockImages\Scaler();
-                $scaler($image);
-                $paletteCreator = new \Studip\StockImages\PaletteCreator();
-                $paletteCreator($image);
-            }
-        }
-
-        $this->cleanup($extractPath);
-        unlink($zipFile);
     }
 
     private function cleanup($path)
