660d0eed2de5dba44375437bd27564b8012016d4
Philipp Schüttlöffel <schuettloeffel@zqs.uni-hannover.de>
2025-08-07 06:47:05 +0000

    Resolve "Wiki Seiten laden teilweise extrem lange"
    Closes #5770
    
    Merge request studip/studip!4401

---
+++ db/migrations/5.5.33_add_index_wikipages_parentid.php
@@ -0,0 +1,20 @@
+<?php
+
+class AddIndexWikipagesParentid extends Migration
+{
+
+    public function description()
+    {
+        return 'Adds index for parent_id to wiki_pages. @see: #5770';
+    }
+
+    public function up()
+    {
+        DBManager::get()->exec("ALTER TABLE `wiki_pages` ADD INDEX parent_id (`parent_id`)");
+    }
+
+    public function down()
+    {
+        DBManager::get()->exec("ALTER TABLE `wiki_pages` DROP INDEX (`parent_id`)");
+    }
+}
