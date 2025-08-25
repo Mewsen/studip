abc663bb1e70e39319341262d7eb18c58952ee6f
Elmar Ludwig <elmar.ludwig@uni-osnabrueck.de>
2025-08-25 17:37:55 +0200

    drop ancient code that breaks tab navigation, fixes #3800
    Closes #3800
    
    Merge request studip/studip!4459

--- resources/assets/javascripts/bootstrap/course_wizard.js
+++ resources/assets/javascripts/bootstrap/course_wizard.js
@@ -5,12 +5,6 @@ STUDIP.domReady(function() {
 });
 
 STUDIP.ready(function() {
-    $('.course-wizard-step-0 *:input:not(input[type=submit])').each(function () {
-        $(this).attr(
-            'tabindex',
-            $(this).closest('section,footer').css('order')
-        );
-    });
     $('#wizard-coursetype').on('change', function() {
         let semtype = $(this).val();
         let mandatory_types = $('#wizard-maxmember').parent('section').data('mandatory');
