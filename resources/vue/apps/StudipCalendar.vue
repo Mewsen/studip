3876b1263080bf9ca0188f9f026fb13cb9b225e8
Moritz Strohm <strohm@data-quest.de>
2025-09-29 12:21:27 +0200

    extended the code of the StudipCalendar vue component

--- resources/vue/apps/StudipCalendar.vue
+++ resources/vue/apps/StudipCalendar.vue
@@ -3,10 +3,13 @@
 </template>
 <script lang="ts">
 import {defineComponent} from 'vue';
+import {CalendarOptions, EventClickArg} from '@fullcalendar/core';
 import FullCalendar from "@fullcalendar/vue3";
-//import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from '@fullcalendar/interaction';
+import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import resourceTimelinePlugin from '@fullcalendar/resource-timeline';
+import Dialog from "../../assets/javascripts/lib/dialog.js";
 
 export default defineComponent({
     name: "StudipCalendar",
@@ -20,8 +23,6 @@ export default defineComponent({
             default: () => ({
                 editable:    false,
                 selectable:  false,
-                studip_urls: [],
-                dialog_size: 'auto',
                 slotMinTime: '08:00',
                 slotMaxTime: '20:00',
                 initialDate: new Date(),
@@ -44,23 +45,63 @@ export default defineComponent({
             type: String,
             required: false,
             default: 'auto'
+        },
+        custom_event_handlers: {
+            type: Object,
+            required: false,
+            default: () => ({})
         }
     },
     data() {
         //Make sure that defaults are set for the calendar:
         let calendar_options = this.config;
-        calendar_options.plugins = [timeGridPlugin, resourceTimelinePlugin];
+        //Add the plugins here so that users of this component
+        //do not need to add them separately.
+        //TODO: load on demand
+        calendar_options.plugins = [dayGridPlugin, timeGridPlugin, resourceTimelinePlugin, interactionPlugin];
         calendar_options.schedulerLicenseKey = 'GPL-My-Project-Is-Open-Source';
 
         if (!calendar_options.initialView) {
             calendar_options.initialView = 'timeGridWeek';
         }
+        console.debug(calendar_options);
+        //Set the event handlers, if needed.
+        if (calendar_options.editable) {
+            calendar_options.eventDrop   = this.handleEventDrop;
+            calendar_options.eventResize = this.handleEventResize;
+
+            if (calendar_options.selectable) {
+                calendar_options.select = this.handleSelection;
+            }
+        }
+        if (this.dialog_urls.view) {
+            calendar_options.eventClick = this.handleEventClick;
+        } else if (this.custom_event_handlers.view) {
+            calendar_options.eventClick = this.custom_event_handlers.view;
+        }
+
         return {
-            calendar_options: calendar_options
+            calendar_options: calendar_options as CalendarOptions,
         }
     },
-    computed: {
-
+    methods: {
+        handleSelection: function(selection: object) {
+            console.debug(selection);
+        },
+        handleEventClick: function(event_data: EventClickArg) {
+            let show_url = event_data.event.extendedProps.studip_view_urls.show;
+            console.debug(show_url);
+            if (show_url) {
+                //Load the dialog:
+                Dialog.fromURL(event_data.event.extendedProps.studip_view_urls.show);
+            }
+        },
+        handleEventDrop: function(event: object) {
+            console.debug(event);
+        },
+        handleEventResize: function(event: object) {
+            console.debug(event);
+        }
     }
 })
 </script>
