b85b9cd0fbe2fb9b2358460d5af475dfd3c14f0a
Jan-Hendrik Willms <tleilax+studip@gmail.com>
2025-10-01 09:51:04 +0200

    allow chosing images from gallery when updating avatars, fixes #5908
    Merge request studip/studip!4504

--- app/views/avatar/update.php
+++ app/views/avatar/update.php
@@ -14,8 +14,7 @@
 
             <label class="file-upload">
                 <?= _('Wählen Sie ein Bild von Ihrer Festplatte aus.') ?>
-                <input type="file" id="avatar-upload" accept="image/gif,image/png,image/jpeg"
-                       capture="camera"
+                <input type="file" id="avatar-upload" accept="image/gif,image/png,image/jpeg,image/webp"
                        data-max-size="<?= Avatar::MAX_FILE_SIZE ?>"
                        data-message-too-large="<?= _('Die hochgeladene Datei ist zu groß. Bitte wählen Sie ein anderes Bild.') ?>">
 
