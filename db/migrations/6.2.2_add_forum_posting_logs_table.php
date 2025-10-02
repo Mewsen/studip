537202e59f49e950c13abbb9ffd2093a10d2bac0
Murtaza Sultani <sultani@data-quest.de>
2025-10-02 13:21:35 +0200

    Add post log and extract post log from admin tag

--- db/migrations/6.2.2_add_forum_posting_logs_table.php
+++ db/migrations/6.2.2_add_forum_posting_logs_table.php
@@ -4,24 +4,15 @@ final class AddForumPostingLogsTable extends Migration
 {
     public function up()
     {
-        \DBManager::get()->exec("
+        DBManager::get()->exec("
             ALTER TABLE `forum_postings`
             ADD COLUMN `editor_id` CHAR(32) COLLATE latin1_bin NOT NULL AFTER `user_id`
-        ");
-
-        \DBManager::get()->exec("
-            UPDATE forum_postings
-                SET content = REGEXP_REPLACE(
-                    content,
-                    '<admin_msg autor=\"[^\"]*\" chdate=\"[^\"]*\">',
-                    ''
-                )
         ");
     }
 
     public function down()
     {
-        \DBManager::get()->exec("
+        DBManager::get()->exec("
             ALTER TABLE `forum_postings` DROP COLUMN `editor_id`
         ");
     }
