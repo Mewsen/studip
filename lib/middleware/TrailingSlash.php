122cec01628695f8a93f5ce00bca9ae3c10572a4
Jan-Hendrik Willms <tleilax+studip@gmail.com>
2025-10-10 16:04:34 +0200

    middleware TrailingSlash should only process GET requests, fixes #5928
    Closes #5928
    
    Merge request studip/studip!4530

--- lib/middleware/TrailingSlash.php
+++ lib/middleware/TrailingSlash.php
@@ -2,8 +2,7 @@
 
 namespace Studip\Middleware;
 
-use Closure;
-use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\ResponseFactoryInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Server\RequestHandlerInterface;
@@ -19,18 +18,18 @@ class TrailingSlash
      *
      * @return ResponseInterface
      */
-    public function __invoke(RequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    public function __invoke(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
-        $uri = $request->getUri();
-        $path = $this->normalize($uri->getPath());
-        if ($this->responseFactory && ($uri->getPath() !== $path)) {
-            return $this->responseFactory->createResponse(301)
-                ->withHeader('Location', (string) $uri->withPath($path));
+        if ($request->getMethod() === 'GET' && $this->responseFactory) {
+            $uri = $request->getUri();
+            $path = $this->normalize($uri->getPath());
+            if ($uri->getPath() !== $path) {
+                return $this->responseFactory->createResponse(301)
+                    ->withHeader('Location', (string) $uri->withPath($path));
+            }
         }
 
-        $response = $handler->handle($request);
-
-        return $response;
+        return $handler->handle($request);
     }
 
     private function normalize(string $path): string
