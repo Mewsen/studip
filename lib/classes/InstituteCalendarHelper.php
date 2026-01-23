577ea08c0db4b71d52a7f80ee51455fc2c1fc3bd
Jan-Hendrik Willms <tleilax+studip@gmail.com>
2026-01-23 16:52:48 +0100

    prevent warning, fixes #6190
    Merge request studip/studip!4688
    
    
    (cherry picked from commit d745254bac99a6e887529dbccdf9b17d321cd0b2)
    
    dee0fa6a prevent warning, fixes #6190
    
    Co-authored-by: Jan-Hendrik Willms <tleilax+studip@gmail.com>

--- lib/classes/InstituteCalendarHelper.php
+++ lib/classes/InstituteCalendarHelper.php
@@ -622,8 +622,8 @@ class InstituteCalendarHelper
                     if (!$GLOBALS['MVV_MODUL']['STATUS']['values'][$modul->stat]['public']) {
                         return false;
                     }
-                    $modul_start = Semester::find($modul->start)->beginn ?: 0;
-                    $modul_end = Semester::find($modul->end)->ende ?: PHP_INT_MAX;
+                    $modul_start = Semester::find($modul->start)->beginn ?? 0;
+                    $modul_end = Semester::find($modul->end)->ende ?? PHP_INT_MAX;
                     return $modul_end >= $course->start_semester->beginn
                         && (
                             $course->isOpenEnded()
