43aafdd8d9cc4f97a7f9017cf669e8f2a19854e4
Thomas Hackl <hackl@data-quest.de>
2026-01-29 10:24:59 +0100

    Resolve "Fehlerhafte URLs bei der Verwaltung der Einrichtungshierarchie"
    Closes #6204
    
    Merge request studip/studip!4697
    
    # Conflicts:
    #	app/controllers/admin/tree.php

--- app/views/admin/tree/rangetree.php
+++ app/views/admin/tree/rangetree.php
@@ -1,9 +1,9 @@
 <div data-studip-tree>
     <studip-tree start-id="<?= htmlReady($startId) ?>" view-type="table" breadcrumb-icon="institute"
                  :with-search="false" :visible-children-only="false"
-                 :editable="true" edit-url="<?= $controller->url_for('admin/tree/edit') ?>"
-                 create-url="<?= $controller->url_for('admin/tree/create') ?>"
-                 delete-url="<?= $controller->url_for('admin/tree/delete') ?>"
+                 :editable="true" edit-url="<?= URLHelper::getURL('dispatch.php/admin/tree/edit', [], true) ?>"
+                 create-url="<?= URLHelper::getURL('dispatch.php/admin/tree/create', [], true) ?>"
+                 delete-url="<?= URLHelper::getURL('dispatch.php/admin/tree/delete', [], true) ?>"
                  :with-courses="true" semester="<?= htmlReady($semester) ?>" :show-structure-as-navigation="true"
                  title="<?= _('Einrichtungshierarchie bearbeiten') ?>"></studip-tree>
 </div>
