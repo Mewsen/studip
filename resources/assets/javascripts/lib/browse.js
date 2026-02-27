4b4835e36535295d221e2c82414aa92c84512f34
Moritz Strohm <strohm@data-quest.de>
2026-02-27 10:30:51 +0000

    removed lib/browse.js, fixes #6302
    Closes #6302
    
    Merge request studip/studip!4774
    
    
    (cherry picked from commit 8b2a37ac3d554aff7495db5e74a8c25dd69df88d)
    
    757b12ca removed lib/browse.js
    
    Co-authored-by: Moritz Strohm <strohm@data-quest.de>

--- resources/assets/javascripts/lib/browse.js
+++
@@ -1,7 +0,0 @@
-const Browse = {
-    selectUser: function(username) {
-        window.location.href = STUDIP.URLHelper.getURL('dispatch.php/profile', { username: username });
-    }
-};
-
-export default Browse;
