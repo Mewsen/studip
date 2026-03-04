0b7738a2fa2cf1b55457603ca7a39fa29dfe70d3
Finn Schneider <schneider@data-quest.de>
2026-03-04 12:40:21 +0100

    qr init

--- app/controllers/course/evaluation.php
+++ app/controllers/course/evaluation.php
@@ -4,6 +4,11 @@ class Course_EvaluationController extends AuthenticatedController
     public function index_action()
     {
         Navigation::activateItem('course/evaluation');
+        $this->evaluations = Questionnaire::findBySQL(
+            "INNER JOIN `questionnaire_eval_assignments` USING(`questionnaire_id`)
+            WHERE `course_id` = ? AND `applied` = 1
+            ORDER BY `questionnaire_eval_assignments`.`startdate` DESC",
+            [Context::getId()]);
         $this->render_template('course/evaluation/index', $this->layout);
     }
 }
--- app/views/course/evaluation/index.php
+++ app/views/course/evaluation/index.php
@@ -1,1 +1,21 @@
 <?php
+/**
+ * @var Course_EvaluationController $controller
+ */
+?>
+
+<?php foreach ($controller->evaluations as $evaluation) : ?>
+
+<?php endforeach ?>
+
+<?php
+if (User::findCurrent()->hasPermissionLevel('tutor', Context::get())) {
+    $actions = new ActionsWidget();
+    $actions->addLink(
+        _("QR-Code für Studierende anzeigen"),
+        URLHelper::getURL('dispatch.php/course/evaluation'),
+        Icon::create("code-qr", "clickable"),
+        ['data-qr-code' => sprintf(_( 'Evaluation zur Veranstaltung %s'), Context::get()->getFullname('number-name'))]
+    );
+    Sidebar::Get()->addWidget($actions);
+}
