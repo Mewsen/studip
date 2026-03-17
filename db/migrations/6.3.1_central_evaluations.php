8ebb4078ad5385f8198c2f8c8d03326ff7f0a030
Finn Schneider <schneider@data-quest.de>
2026-03-17 13:11:58 +0100

    add result_visible_for & minimum_responses to questionnaire

--- db/migrations/6.3.1_central_evaluations.php
+++ db/migrations/6.3.1_central_evaluations.php
@@ -15,6 +15,8 @@ class CentralEvaluations extends Migration
             ADD COLUMN `is_template` tinyint(1) NOT NULL DEFAULT 0 AFTER `user_id`,
             ADD COLUMN `template_is_enabled` tinyint(1) DEFAULT NULL AFTER `is_template`,
             ADD COLUMN `template_id` char(32) DEFAULT NULL AFTER `questionnaire_id`,
+            ADD COLUMN `result_visible_for` enum('autor','tutor','dozent') DEFAULT NULL,
+            ADD COLUMN `minimum_responses` int(11) unsigned NOT NULL DEFAULT 0,
             ADD KEY `template_id` (`template_id`)
         ");
 
@@ -176,7 +178,9 @@ class CentralEvaluations extends Migration
             ALTER TABLE `questionnaires`
             DROP COLUMN `is_template`,
             DROP COLUMN `template_is_enabled`,
-            DROP COLUMN `template_id`
+            DROP COLUMN `template_id`,
+            DROP COLUMN `result_visible_for`,
+            DROP COLUMN `minimum_responses`
         ");
 
         DBManager::get()->exec("
