3c3b307bdbf24a49c490ad2f747bedcb4ed8fb85
Jan-Hendrik Willms <tleilax+studip@gmail.com>
2026-04-21 11:12:16 +0200

    cache by plugin/module as well and reduce clear method (since it's currently unused anyway), fixes #6500
    Closes #6500
    
    Merge request studip/studip!4931

--- lib/modules/IconNavigationCache.php
+++ lib/modules/IconNavigationCache.php
@@ -13,7 +13,7 @@ final class IconNavigationCache
 {
     private static bool $enabled = true;
 
-    /** @var array<string, array<string, ?Navigation>> */
+    /** @var array<string, array<string, array<string, ?Navigation>>> */
     private static array $cache = [];
 
     /**
@@ -29,56 +29,45 @@ final class IconNavigationCache
     }
 
     /**
-     * Clear the cache, optionally for a specific user or course.
+     * Clear the cache, optionally for a specific user.
      */
-    public static function clear(?string $user_id = null, ?string $course_id = null): void
-    {
-        if ($user_id !== null && $course_id !== null) {
-            unset(self::$cache[$user_id][$course_id]);
-        } elseif ($user_id !== null) {
+    public static function clear(?string $user_id = null): void {
+        if ($user_id !== null) {
             unset(self::$cache[$user_id]);
-        } elseif ($course_id !== null) {
-            self::$cache = array_map(
-                fn(array $user_cache) => array_filter(
-                    $user_cache,
-                    fn(string $id): bool => $id !== $course_id,
-                    ARRAY_FILTER_USE_KEY
-                ),
-                self::$cache
-            );
-            self::$cache = array_filter(
-                self::$cache,
-                fn(array $user_cache): bool => $user_cache !== [],
-            );
         } else {
             self::$cache = [];
         }
     }
 
-    public static function has(string $user_id, string $course_id): bool
+    public static function has(string $user_id, string $course_id, string $plugin_id): bool
     {
         if (!self::$enabled) {
             return false;
         }
-        return array_key_exists($course_id, self::$cache[$user_id] ?? []);
+        return array_key_exists($plugin_id, self::$cache[$user_id][$course_id] ?? []);
     }
 
-    public static function get(string $user_id, string $course_id): ?Navigation
+    public static function get(string $user_id, string $course_id, string $plugin_id): ?Navigation
     {
         if (!self::$enabled) {
             return null;
         }
-        return self::$cache[$user_id][$course_id] ?? null;
+        return self::$cache[$user_id][$course_id][$plugin_id] ?? null;
     }
 
-    public static function set(string $user_id, string $course_id, ?Navigation $nav): ?Navigation
-    {
+    public static function set(
+        string $user_id,
+        string $course_id,
+        string $plugin_id,
+        ?Navigation $nav
+    ): ?Navigation {
         if (!self::$enabled) {
             return $nav;
         }
 
         self::$cache[$user_id] ??= [];
+        self::$cache[$user_id][$course_id] ??= [];
 
-        return self::$cache[$user_id][$course_id] = $nav;
+        return self::$cache[$user_id][$course_id][$plugin_id] = $nav;
     }
 }
