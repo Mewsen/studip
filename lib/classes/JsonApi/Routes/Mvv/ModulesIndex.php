ba0ec9f9070875aeaf6b107bd5034da3b8a3b51f
Jan-Hendrik Willms <tleilax+studip@gmail.com>
2026-05-29 16:52:53 +0200

    fix pagination of modules route, fixes #6576
    Closes #6576
    
    Merge request studip/studip!5012

--- lib/classes/JsonApi/Routes/Mvv/ModulesIndex.php
+++ lib/classes/JsonApi/Routes/Mvv/ModulesIndex.php
@@ -5,8 +5,11 @@ namespace JsonApi\Routes\Mvv;
 use JsonApi\Errors\AuthorizationFailedException;
 use JsonApi\Errors\BadRequestException;
 use JsonApi\Schemas\Module;
-use Psr\Http\Message\ServerRequestInterface as Request;
-use Psr\Http\Message\ResponseInterface as Response;
+use Modul;
+use Psr\Http\Message\{
+    ServerRequestInterface as Request,
+    ResponseInterface as Response
+};
 use JsonApi\JsonApiController;
 
 class ModulesIndex extends JsonApiController
@@ -28,9 +31,9 @@ class ModulesIndex extends JsonApiController
     /**
      * @SuppressWarnings(PHPMD.UnusedFormalParameters)
      */
-    public function __invoke(Request $request, Response $response, $args)
+    public function __invoke(Request $request, Response $response, $args): Response
     {
-        if (!Authority::canIndexModules($user = $this->getUser($request))) {
+        if (!Authority::canIndexModules($this->getUser($request))) {
             throw new AuthorizationFailedException();
         }
 
@@ -41,11 +44,9 @@ class ModulesIndex extends JsonApiController
         }
 
         [$offset, $limit] = $this->getOffsetAndLimit();
-        $modules = $this->getModules($filtering, $offset, $limit);
-
         return $this->getPaginatedContentResponse(
-            $modules,
-            count($modules)
+            $this->getModules($filtering, $offset, $limit),
+            $this->countModules($filtering)
         );
     }
 
@@ -78,43 +79,69 @@ class ModulesIndex extends JsonApiController
         }
     }
 
-    private function getModules($filtering, $offset, $limit): array
+    private function countModules(array $filtering): int
     {
+        [$condition, $parameters] = $this->getConditionAndParameters($filtering);
+
+        return Modul::countBySql(
+            $condition,
+            $parameters
+        );
+    }
+
+    private function getModules(array $filtering, int $offset, int $limit): array
+    {
+        [$condition, $parameters] = $this->getConditionAndParameters($filtering);
+
+        $condition .= ' ORDER BY `mvv_modul`.`code` ASC, `mvv_modul_deskriptor`.`bezeichnung` ASC
+                    LIMIT :limit OFFSET :offset';
+        $parameters[':offset'] = $offset;
+        $parameters[':limit'] = $limit;
+
+        return Modul::findBySQL($condition, $parameters);
+    }
+
+    private function getConditionAndParameters(array $filtering): array
+    {
+        $parameters = [];
+
         $join = '';
         $where = ' 1 ';
-        $filtering['offset'] = $offset;
-        $filtering['limit'] = $limit;
         if (isset($filtering['institute'])) {
             $where .= ' AND `institut_id` = :institute ';
+            $parameters[':institute'] = $filtering['institute'];
         }
         if (isset($filtering['section'])) {
             $join .= 'LEFT JOIN `mvv_stgteilabschnitt_modul` USING(`modul_id`) ';
             $where .= ' AND `mvv_stgteilabschnitt_modul`.`abschnitt_id` = :section';
+
+            $parameters[':section'] = $filtering['section'];
         }
         if (isset($filtering['semester'])) {
             $semester = \Semester::find($filtering['semester']);
-            unset($filtering['semester']);
-            $filtering['semester_start'] = $semester->beginn;
-            $filtering['semester_end'] = $semester->ende;
             $join .= 'LEFT JOIN `semester_data` AS `start_sem`
                         ON (`mvv_modul`.`start` = `start_sem`.`semester_id`)
                     LEFT JOIN `semester_data` AS `end_sem`
                         ON (`mvv_modul`.`end` = `end_sem`.`semester_id`) ';
             $where .= ' AND (`start_sem`.`beginn` <= :semester_end OR ISNULL(`start_sem`.`beginn`))
                         AND (`end_sem`.`ende` >= :semester_start OR ISNULL(`end_sem`.`ende`))';
+
+            $parameters[':semester_start'] = $semester->beginn;
+            $parameters[':semester_end'] = $semester->ende;
         }
         $join .= 'LEFT JOIN `mvv_modul_deskriptor` USING(`modul_id`) ';
         if (isset($filtering['q'])) {
             $where .= " AND (`mvv_modul_deskriptor`.`bezeichnung` LIKE CONCAT('%', :q, '%') OR `mvv_modul`.`code` LIKE CONCAT('%', :q, '%')) ";
+            $parameters[':q'] = $filtering['q'];
         }
         if (isset($filtering['stat'])) {
             $where .= " AND `stat` = :stat ";
+            $parameters[':stat'] = $filtering['stat'];
         }
-        $where .= ' ORDER BY `mvv_modul`.`code` ASC, `mvv_modul_deskriptor`.`bezeichnung` ASC
-                    LIMIT :limit OFFSET :offset';
-        return \Modul::findBySQL(
-            ($join ? $join . ' WHERE ' : '') . $where,
-            $filtering
-        );
+
+        return [
+            $join ? $join . ' WHERE ' . $where : $where,
+            $parameters
+        ];
     }
 }
